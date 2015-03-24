09874d5e4a7579e6057a48ce97419a0657d9add2
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-03-23 17:47:30 -0700

    add sample unit test

---
+++ aspnet/test/ApplicationInsights.AspNet.Tests/Sample.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Microsoft.ApplicationInsights.AspNet.Tests
+{
+	using System;
+	using Xunit;
+
+	public class Sample
+    {
+        [Fact]
+        public void TrueIsTrue()
+        {
+			Assert.True(true);
+        }
+    }
+}
+ 
