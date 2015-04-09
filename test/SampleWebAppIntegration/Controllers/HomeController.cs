97652211a3d423db49296eaa4b22a1f7f4c9a55a
Upendra Singh <upendras@microsoft.com>
2015-04-09 13:56:17 -0700

    Add functional test when application traces async
    Add functional test when application traces Trace, Event, Metric
    asynchronously

--- test/SampleWebAppIntegration/Controllers/HomeController.cs
+++ test/SampleWebAppIntegration/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Runtime;
 
 namespace SampleWebAppIntegration.Controllers
 {
@@ -19,9 +24,9 @@ namespace SampleWebAppIntegration.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = await this.GetContactDetails();
 
             return View();
         }
@@ -30,5 +35,21 @@ namespace SampleWebAppIntegration.Controllers
         {
             return View("~/Views/Shared/Error.cshtml");
         }
-    }
+
+		private async Task<string> GetContactDetails()
+		{
+			var applicationEnvironment = (IApplicationEnvironment)this.Resolver.GetService(typeof(IApplicationEnvironment));
+			var contactFilePath = Path.Combine(applicationEnvironment.ApplicationBasePath, "contact.txt");
+			var telemetryClient = (TelemetryClient)this.Resolver.GetService(typeof(TelemetryClient));
+			telemetryClient.TrackEvent("GetContact");
+
+			using (var reader = System.IO.File.OpenText(contactFilePath))
+			{
+				var contactDetails = await reader.ReadToEndAsync();
+				telemetryClient.TrackMetric("ContactFile", 1);
+				telemetryClient.TrackTrace("Fetched contact details.", SeverityLevel.Information);
+				return contactDetails;
+			}
+		}
+	}
 }
--- test/SampleWebAppIntegration/FunctionalTest/SuccessScenario.cs
+++ test/SampleWebAppIntegration/FunctionalTest/SuccessScenario.cs
@@ -1,61 +1,90 @@
 ﻿namespace SampleWebAppIntegration.FunctionalTest
 {
-    using Xunit;
-    using System.Net.Http;
-    using System.Net;
-    using FunctionalTestUtils;
-    using System.Collections.Generic;
-    using Microsoft.ApplicationInsights.Channel;
-    using Microsoft.ApplicationInsights.DataContracts;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Net;
+	using System.Net.Http;
+	using System.Threading.Tasks;
+	using FunctionalTestUtils;
+	using Microsoft.ApplicationInsights.Channel;
+	using Microsoft.ApplicationInsights.DataContracts;
+	using Xunit;
 
-    public class SuccessScenario
+	public class SuccessScenario
     {
         [Fact]
         public async void ServerReturns200WithMonitoring()
         {
-            InProcessServer server = new InProcessServer("SampleWebAppIntegration");
+			Action<IList<ITelemetry>> assertions = (telemetries) =>
+			{
+				Assert.Equal(1, telemetries.Count);
+				ITelemetry telemetry = telemetries[0];
+				Assert.IsAssignableFrom(typeof(RequestTelemetry), telemetry);
 
-            List<ITelemetry> buffer = new List<ITelemetry>();
-            BackTelemetryChannelExtensions.InitializeFunctionalTestTelemetryChannel(buffer);
+				RequestTelemetry request = (RequestTelemetry)telemetry;
+				Assert.Equal("200", request.ResponseCode);
+				Assert.Equal("GET /", request.Name);
+			};
 
-            using (server.Start())
-            {
-                HttpClient client = new HttpClient();
-                var result = await client.GetAsync(server.BaseHost);
-                Assert.Equal(HttpStatusCode.OK, result.StatusCode);
-
-                Assert.Equal(1, buffer.Count);
-                ITelemetry telemetry = buffer[0];
-                Assert.IsAssignableFrom(typeof(RequestTelemetry), telemetry);
-
-                RequestTelemetry request = (RequestTelemetry)telemetry;
-                Assert.Equal("200", request.ResponseCode);
-                Assert.Equal("GET /", request.Name);
-            }
+			await this.ExecuteRequest("/", HttpStatusCode.OK, assertions);
         }
 
         [Fact]
         public async void ServerReturns404ForNotExistingController()
         {
-            InProcessServer server = new InProcessServer("SampleWebAppIntegration");
-
-            List<ITelemetry> buffer = new List<ITelemetry>();
-            BackTelemetryChannelExtensions.InitializeFunctionalTestTelemetryChannel(buffer);
-
-            using (server.Start())
-            {
-                HttpClient client = new HttpClient();
-                var result = await client.GetAsync(server.BaseHost + "/not/existing/controller");
-                Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
-
-                Assert.Equal(1, buffer.Count);
-                ITelemetry telemetry = buffer[0];
+			Action<IList<ITelemetry>> assertions = (telemetries) =>
+			{
+				Assert.Equal(1, telemetries.Count);
+                ITelemetry telemetry = telemetries[0];
                 Assert.IsAssignableFrom(typeof(RequestTelemetry), telemetry);
 
                 RequestTelemetry request = (RequestTelemetry)telemetry;
                 Assert.Equal("404", request.ResponseCode);
-            }
+			};
+
+			await this.ExecuteRequest("/not/existing/controller", HttpStatusCode.NotFound, assertions);
         }
 
-    }
+		[Fact]
+		public async void ServerReturns200FromAsyncController()
+		{
+			Action<IList<ITelemetry>> assertions = (telemetries) =>
+			{
+				Assert.Equal(4, telemetries.Count);
+				RequestTelemetry request = telemetries.OfType<RequestTelemetry>().Single();
+				Assert.Equal("200", request.ResponseCode);
+
+				EventTelemetry eventTelemetry = telemetries.OfType<EventTelemetry>().Single();
+				Assert.Equal("GetContact", eventTelemetry.Name);
+
+				MetricTelemetry metricTelemetry = telemetries.OfType<MetricTelemetry>().Single();
+				Assert.Equal(1, metricTelemetry.Count);
+				Assert.Equal("ContactFile", metricTelemetry.Name);
+				Assert.Equal(1, metricTelemetry.Value);
+
+				TraceTelemetry traceTelemetry = telemetries.OfType<TraceTelemetry>().Single();
+				Assert.Equal("GET /home/contact", traceTelemetry.Context.Operation.Name);
+			};
+
+			await this.ExecuteRequest("/home/contact", HttpStatusCode.OK, assertions);
+		}
+
+		private async Task ExecuteRequest(string relativeRequestAddress, HttpStatusCode expectedStatus, Action<List<ITelemetry>> assertions)
+		{
+			InProcessServer server = new InProcessServer("SampleWebAppIntegration");
+
+			List<ITelemetry> buffer = new List<ITelemetry>();
+			BackTelemetryChannelExtensions.InitializeFunctionalTestTelemetryChannel(buffer);
+
+			using (server.Start())
+			{
+				HttpClient client = new HttpClient();
+				var result = await client.GetAsync(server.BaseHost + relativeRequestAddress);
+				Assert.Equal(expectedStatus, result.StatusCode);
+
+				assertions.Invoke(buffer);
+			}
+		}
+	}
 }
