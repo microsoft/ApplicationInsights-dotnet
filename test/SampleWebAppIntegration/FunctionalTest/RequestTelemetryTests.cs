7da9448b37e734880ea07d62022d725e3c7c4dc9
Anastasia Baranchenkova <abaranch@microsoft.com>
2015-04-11 22:56:38 +0000

    Move tests added after merge in RequestTelemetryTests class

--- test/SampleWebAppIntegration/FunctionalTest/RequestTelemetryTests.cs
+++ test/SampleWebAppIntegration/FunctionalTest/RequestTelemetryTests.cs
@@ -2,6 +2,7 @@
 {
     using FunctionalTestUtils.Tests;
     using Microsoft.ApplicationInsights.DataContracts;
+    using System.Linq;
     using Xunit;
 
     public class RequestTelemetryTests : RequestTelemetryTestsBase
@@ -27,12 +28,12 @@
         {
             var expectedRequestTelemetry = new RequestTelemetry();
             expectedRequestTelemetry.HttpMethod = "GET";
-            expectedRequestTelemetry.Name = "GET Home/Contact [id]";
+            expectedRequestTelemetry.Name = "GET Home/About [id]";
             expectedRequestTelemetry.ResponseCode = "200";
             expectedRequestTelemetry.Success = true;
             // expectedRequestTelemetry.Url ???
 
-            this.ValidateBasicRequest("/Home/Contact/5", expectedRequestTelemetry);
+            this.ValidateBasicRequest("/Home/About/5", expectedRequestTelemetry);
         }
 
         [Fact]
@@ -45,6 +46,24 @@
             expectedRequestTelemetry.Success = false;
 
             this.ValidateBasicRequest("/not/existing/controller", expectedRequestTelemetry);
+        }
+
+        [Fact]
+        public void TestMixedTelemetryItemsReceived()
+        {
+            var task = this.HttpClient.GetAsync(this.Server.BaseHost + "/home/contact");
+            task.Wait(TestTimeoutMs);
+
+            var request = this.Buffer.OfType<RequestTelemetry>().Single();
+            var eventTelemetry = this.Buffer.OfType<EventTelemetry>().Single();
+            var metricTelemetry = this.Buffer.OfType<MetricTelemetry>().Single();
+            var traceTelemetry = this.Buffer.OfType<TraceTelemetry>().Single();
+
+            Assert.Equal(4, this.Buffer.Count);
+            Assert.NotNull(request);
+            Assert.NotNull(eventTelemetry);
+            Assert.NotNull(metricTelemetry);
+            Assert.NotNull(traceTelemetry);
         }
     }
 }
