32ce48d42ba6503adb94b5157d44b31ca4ecd7ec
Anastasia Baranchenkova <abaranch@microsoft.com>
2015-04-17 14:15:21 -0700

    PerfTest solution

---
+++ test/PerfTest/Aspnet5_EmptyApp_46/CycleBusinessLogic.cs
@@ -0,0 +1,34 @@
+﻿namespace Aspnet5_EmptyApp_46
+{
+    using System;
+    
+    public class CycleBusinessLogic
+    {
+        private readonly int timeToCycleInMs;
+        private DateTime logicTime;
+
+        public CycleBusinessLogic(int timeToCycleInMs)
+        {
+            this.timeToCycleInMs = timeToCycleInMs;
+        }
+
+        public DateTime LogicTime
+        {
+            get
+            {
+                return this.logicTime;
+            }
+        }
+
+        public void Execute()
+        {
+            DateTime finishTime = DateTime.Now.AddMilliseconds(this.timeToCycleInMs);
+            this.logicTime = DateTime.Now;
+
+            while (logicTime < finishTime)
+            {
+                this.logicTime = DateTime.Now;
+            }
+        }
+    }
+}
