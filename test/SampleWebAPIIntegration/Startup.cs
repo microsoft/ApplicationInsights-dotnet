11ccc0f26635d33f745ed312f884246ac48c1458
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-04-21 19:42:55 -0700

    Add Application Insights into Web API project

--- test/SampleWebAPIIntegration/Startup.cs
+++ test/SampleWebAPIIntegration/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.AspNet;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.ConfigurationModel;
 
 namespace SampleWebAPIIntegration
 {
@@ -14,12 +16,21 @@ namespace SampleWebAPIIntegration
     {
         public Startup(IHostingEnvironment env)
         {
+            // Setup configuration sources.
+            Configuration = new Configuration()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
         }
+
+        public IConfiguration Configuration { get; set; }
 
         // This method gets called by a runtime.
         // Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Application Insights services to the services container.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
             services.AddMvc();
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
@@ -29,6 +40,19 @@ namespace SampleWebAPIIntegration
         // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Add the following to the request pipeline only in development environment.
+            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+            {
+                // Set immediate delivery for Application Insights events.
+                app.SetApplicationInsightsTelemetryDeveloperMode();
+            }
+
+            // Add Application Insights monitoring to the request pipeline as a very first middleware.
+            app.UseApplicationInsightsRequestTelemetry();
+
+            // Add Application Insights exceptions handling to the request pipeline.
+            app.UseApplicationInsightsExceptionTelemetry();
+
             // Configure the HTTP request pipeline.
             app.UseStaticFiles();
 
