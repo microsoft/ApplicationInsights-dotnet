bb3a5291d3adcdfee8489a80732776ad9f74a79d
Oleg Sych <olegsych@microsoft.com>
2015-04-23 15:31:33 -0700

    - Remove unnecessary using directive for Microsoft.ApplicationInsights.AspNet and other namespaces.

--- test/WebApiShimFw46.FunctionalTests/Startup.cs
+++ test/WebApiShimFw46.FunctionalTests/Startup.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.ApplicationInsights.AspNet;
+using FunctionalTestUtils;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
-using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.ConfigurationModel;
-using FunctionalTestUtils;
+using Microsoft.Framework.DependencyInjection;
 
 namespace SampleWebAPIIntegration
 {
