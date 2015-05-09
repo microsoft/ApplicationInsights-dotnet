ba1ae3d3ec8727e4a0e2edf77eefd894f216167c
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-05-08 19:53:22 -0700

    make functional tests pass all the time

--- test/FunctionalTestUtils/TelemetryTestsBase.cs
+++ test/FunctionalTestUtils/TelemetryTestsBase.cs
@@ -1,96 +1,52 @@
 ﻿namespace FunctionalTestUtils
 {
     using System;
+    using System.Diagnostics;
     using System.Linq;
-    using System.Collections.Generic;
     using System.Net.Http;
-    using Microsoft.ApplicationInsights.Channel;
     using Microsoft.ApplicationInsights.DataContracts;
     using Xunit;
 
-    public abstract class TelemetryTestsBase : IDisposable
+    public abstract class TelemetryTestsBase
     {
         protected const int TestTimeoutMs = 5000;
 
-        private InProcessServer server;
-        private IList<ITelemetry> buffer = new List<ITelemetry>();
-        private HttpClient client = new HttpClient();
-
-        public TelemetryTestsBase(string assemblyName)
-        {
-            this.server = new InProcessServer(assemblyName);
-            BackTelemetryChannelExtensions.InitializeFunctionalTestTelemetryChannel(buffer);
-        }
-
-        public IList<ITelemetry> Buffer
-        {
-            get
-            {
-                return this.buffer;
-            }
-        }
-
-        public InProcessServer Server
-        {
-            get
-            {
-                return this.server;
-            }
-        }
-
-        public HttpClient HttpClient
-        {
-            get
-            {
-                return this.client;
-            }
-        }
-
-        public void ValidateBasicRequest(string requestPath, RequestTelemetry expected)
+        public void ValidateBasicRequest(InProcessServer server, string requestPath, RequestTelemetry expected)
         {
             DateTimeOffset testStart = DateTimeOffset.Now;
-            var task = this.HttpClient.GetAsync(this.Server.BaseHost + requestPath);
+            var timer = Stopwatch.StartNew();
+            var httpClient = new HttpClient();
+            var task = httpClient.GetAsync(server.BaseHost + requestPath);
             task.Wait(TestTimeoutMs);
             var result = task.Result;
 
-            var actual = this.Buffer.OfType<RequestTelemetry>().Single();
+            var actual = server.BackChannel.Buffer.OfType<RequestTelemetry>().Single();
 
+            timer.Stop();
             Assert.Equal(expected.ResponseCode, actual.ResponseCode);
             Assert.Equal(expected.Name, actual.Name);
             Assert.Equal(expected.Success, actual.Success);
             Assert.Equal(expected.Url, actual.Url);
             Assert.InRange<DateTimeOffset>(actual.Timestamp, testStart, DateTimeOffset.Now);
-            Assert.True(actual.Duration < (DateTimeOffset.Now - testStart), "duration");
+            Assert.True(actual.Duration < timer.Elapsed, "duration");
             Assert.Equal(expected.HttpMethod, actual.HttpMethod);
         }
 
-        public void ValidateBasicException(string requestPath, ExceptionTelemetry expected)
+        public void ValidateBasicException(InProcessServer server, string requestPath, ExceptionTelemetry expected)
         {
             DateTimeOffset testStart = DateTimeOffset.Now;
-            var task = this.HttpClient.GetAsync(this.Server.BaseHost + requestPath);
+            var httpClient = new HttpClient();
+            var task = httpClient.GetAsync(server.BaseHost + requestPath);
             task.Wait(TestTimeoutMs);
             var result = task.Result;
 
-            var actual = this.Buffer.OfType<ExceptionTelemetry>().Single();
+            var actual = server.BackChannel.Buffer.OfType<ExceptionTelemetry>().Single();
 
             Assert.Equal(expected.Exception.GetType(), actual.Exception.GetType());
             Assert.NotEmpty(actual.Exception.StackTrace);
             Assert.Equal(actual.HandledAt, actual.HandledAt);
             Assert.NotEmpty(actual.Context.Operation.Name);
             Assert.NotEmpty(actual.Context.Operation.Id);
-        }
-
-        public void Dispose()
-        {
-            if (this.server != null)
-            {
-                this.server.Dispose();
-            }
-
-            if (this.client != null)
-            {
-                this.client.Dispose();
-            }
         }
     }
 }
