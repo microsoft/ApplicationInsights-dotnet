9d776dd47dc6bcad8ab6e2d78f36d2d67abf0c6c
Sergei Nikitin <sergeyni@microsoft.com>
2015-05-21 16:28:12 -0700

    Powershell script to run unit tests during the build

--- RunTests.ps1
+++ RunTests.ps1
@@ -15,7 +15,7 @@ Function Execute-Process {
 		[String]$Arguments
 	)
 
-	$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+	$pinfo = New-Object System.Diagnostics.ProcessStartInfo;
 	$pinfo.FileName = $dnxPath;
 	$pinfo.RedirectStandardError = $true;
 	$pinfo.RedirectStandardOutput = $true;
@@ -30,14 +30,46 @@ Function Execute-Process {
 	Return $p.StandardOutput.ReadToEnd();
 }
 
+Function Get-OutputSummary {
+	Param (
+		[String]$Data
+	)
+
+	If ($Data -match '\W*Errors:\W*(?<Errors>\d+),\W*Failed:\W*(?<Failed>\d+)') {
+		Return New-Object PSObject -Property @{
+			Errors = [int]$matches['Errors'];
+			Failed = [int]$matches['Failed'];
+		};	
+	} Else {
+		Throw "Input string is not wellformet to extract summary data";
+	}
+}
+
+[PSObject[]]$global:failed = @();
 $DnxRuntimes |% {
 	$dnxPath = "%USERPROFILE%\.dnx\runtimes\$($_)\bin\dnx.exe";
 	$dnxPath = [System.Environment]::ExpandEnvironmentVariables($dnxPath);
 
 	$TestProjects |% {
 		[String]$arguments = "$_ test -nologo -diagnostics";
-		[String]$Out = Execute-Process $dnxPath $arguments;
 
-		Write-Host $Out;
+		Write-Host "=========================================================";
+		Write-Host "== Executing tests";
+		Write-Host "== Working Folder: $((pwd).Path)";
+		Write-Host "== Runtime:$dnxPath";
+		Write-Host "== Args:$arguments";
+		Write-Host "=========================================================";
+
+		[String]$out = Execute-Process $dnxPath $arguments;
+		Write-Host $out;
+
+		$summary = Get-OutputSummary $Out;
+		If (($summary.Failed -ne 0) -or ($summary.Errors -ne 0)){
+			$global:failed += $summary;
+		}
 	}
+}
+
+If ($global:failed.Count -gt 0){
+	Throw "Test execution failed";
 }
