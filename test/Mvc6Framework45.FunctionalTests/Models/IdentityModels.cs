be70fcdb54d01d469b7f576e1e34f9a73808bd88
Karthik Tangirala <kartang@microsoft.com>
2015-06-26 10:24:54 -0700

    Multiple Builds Based on Nuget Feed

--- test/Mvc6Framework45.FunctionalTests/Models/IdentityModels.cs
+++ test/Mvc6Framework45.FunctionalTests/Models/IdentityModels.cs
@@ -24,7 +24,8 @@ namespace Mvc6Framework45.FunctionalTests.Models
             // Create the database and schema if it doesn't exist
             if (!_created)
             {
-                Database.AsRelational().ApplyMigrations();
+                Database.ApplyMigrations();
+                //Database.AsRelational().ApplyMigrations();
                 _created = true;
             }
         }
