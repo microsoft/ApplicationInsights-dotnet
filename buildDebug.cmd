9f99eac2eb5df76df9490bba7969a2a5dd6cfd4e
Karthik Tangirala <kartang@microsoft.com>
2015-06-30 15:22:02 -0700

    Generating two versions of nuget packages with and without pre-release version

--- buildDebug.cmd
+++ buildDebug.cmd
@@ -10,6 +10,6 @@ SET Platform=Any CPU
 SET NugetOrg_Feed=https://www.nuget.org/api/v2/
 SET IsOfficialBuild=False
 SET DefaultFeed=https://www.myget.org/F/aspnetvnext/api/v2/
-msbuild dirs.proj /nologo /m:1  /fl /flp:logfile="%ProjectName%.%Platform%.log";v=d /flp1:logfile="%ProjectName%.%Platform%.wrn";warningsonly /flp2:logfile="%ProjectName%.%Platform%.err";errorsonly /p:Configuration=%Configuration% /p:Platform="%Platform%" /p:RunCodeAnalysis="False" /flp3:logfile="%ProjectName%.%Platform%.prf";performancesummary /flp4:logfile="%ProjectName%.%Platform%.exec.log";showcommandline /p:BuildSingleFilePackage=true /p:IsOfficialBuild=False /p:TF_BUILD_BUILDNUMBER=22222
-PAUSE
+
+msbuild dirs.proj /nologo /m:1  /fl /flp:logfile="%ProjectName%.%Platform%.log";v=d /flp1:logfile="%ProjectName%.%Platform%.wrn";warningsonly /flp2:logfile="%ProjectName%.%Platform%.err";errorsonly /p:Configuration=%Configuration% /p:Platform="%Platform%" /p:RunCodeAnalysis="False" /flp3:logfile="%ProjectName%.%Platform%.prf";performancesummary /flp4:logfile="%ProjectName%.%Platform%.exec.log";showcommandline /p:BuildSingleFilePackage=true /p:IsOfficialBuild=False
 
