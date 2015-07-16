021f4c327b4757be5c4abea3b17245c80493cea5
Meni Zalzman <meniz@microsoft.com>
2015-07-16 10:12:36 -0700

    - Changing performance counter telemetry to public type - Clean the internal visible to list

--- src/Core/Managed/Shared/DataContracts/PerformanceCounterTelemetry.cs
+++ src/Core/Managed/Shared/DataContracts/PerformanceCounterTelemetry.cs
@@ -12,7 +12,7 @@
     /// The class that represents information about performance counters.
     /// </summary>
     [DebuggerDisplay(@"CategoryName={CategoryName}; CounterName={CounterName}; InstanceName={InstanceName}; Value={Value}; Timestamp={Timestamp}")]
-    internal class PerformanceCounterTelemetry : ITelemetry, ISupportProperties
+    public class PerformanceCounterTelemetry : ITelemetry, ISupportProperties
     {
         internal const string TelemetryName = "PerformanceCounter";
         internal readonly string BaseType = typeof(PerformanceCounterData).Name;
