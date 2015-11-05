14dbc1c104fe468c3343684dd57c1e4a5894317e
Sergey Kanzhelev <SergKanz@microsoft.com>
2015-11-05 10:35:56 -0800

    Add Operation telemetry initializer by default

--- Test/CoreSDK.Test/Shared/Extensibility/Implementation/OperationContextTest.cs
+++ Test/CoreSDK.Test/Shared/Extensibility/Implementation/OperationContextTest.cs
@@ -1,19 +1,13 @@
 ﻿namespace Microsoft.ApplicationInsights.Extensibility.Implementation
 {
-    using System;
     using System.Collections.Generic;
-    using System.Globalization;
-    using System.IO;
     using System.Reflection;
-    using Microsoft.ApplicationInsights.DataContracts;
 #if WINDOWS_PHONE || WINDOWS_STORE
     using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 #else
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 #endif
     using Assert = Xunit.Assert;
-    using EndpointOperationContext = Microsoft.Developer.Analytics.DataCollection.Model.v2.OperationContextData;
-    using JsonConvert = Newtonsoft.Json.JsonConvert;
 
     [TestClass]
     public class OperationContextTest
