1f1e33c4ff07aca289468bba80e562c0d106b979
abaranch <abaranch@microsoft.com>
2015-12-28 15:36:18 -0800

    Telemetry Processors Design Update

--- src/Core/Managed/Shared/Extensibility/TelemetryConfiguration.cs
+++ src/Core/Managed/Shared/Extensibility/TelemetryConfiguration.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Threading;
     using Microsoft.ApplicationInsights.Channel;
     using Microsoft.ApplicationInsights.DataContracts;
@@ -24,6 +25,7 @@
         private TelemetryProcessorChain telemetryProcessorChain;
         private string instrumentationKey = string.Empty;
         private bool disableTelemetry = false;
+        private TelemetryProcessorChainBuilder builder;
 
         /// <summary>
         /// Gets the active <see cref="TelemetryConfiguration"/> instance loaded from the ApplicationInsights.config file. 
@@ -131,22 +133,54 @@
         }
 
         /// <summary>
-        /// Gets the first telemetry processor in the chain of processors.
+        /// Gets a readonly collection of TelemetryProcessors.
         /// </summary>
-        public TelemetryProcessorChain TelemetryProcessors
+        public ReadOnlyCollection<ITelemetryProcessor> TelemetryProcessors
+        {
+            get
+            {
+                return new ReadOnlyCollection<ITelemetryProcessor>(this.TelemetryProcessorChain.TelemetryProcessors);
+            }
+        }
+
+        /// <summary>
+        /// Gets the TelemetryProcessorChainBuilder which can build and populate TelemetryProcessors in the TelemetryConfiguration.
+        /// </summary>
+        public TelemetryProcessorChainBuilder TelemetryProcessorChainBuilder
+        {
+            get
+            {
+                LazyInitializer.EnsureInitialized(ref this.builder, () => new TelemetryProcessorChainBuilder(this));
+                return this.builder;
+            }
+
+            internal set
+            {
+                this.builder = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the telemetry channel.
+        /// </summary>
+        public ITelemetryChannel TelemetryChannel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chain of processors.
+        /// </summary>
+        internal TelemetryProcessorChain TelemetryProcessorChain
         {
             get
             {
                 if (this.telemetryProcessorChain == null)
                 {
-                    // Gets the telemetryprocessorchainbuilder, builds the processor chain and sets the same in the configuration.
-                    this.GetTelemetryProcessorChainBuilder().Build();
+                    this.TelemetryProcessorChainBuilder.Build();
                 }
 
                 return this.telemetryProcessorChain;
             }
 
-            internal set
+            set
             {
                 if (value == null)
                 {
@@ -156,11 +190,6 @@
                 this.telemetryProcessorChain = value;
             }
         }
-
-        /// <summary>
-        /// Gets or sets the telemetry channel.
-        /// </summary>
-        public ITelemetryChannel TelemetryChannel { get; set; }
 
         /// <summary>
         /// Creates a new <see cref="TelemetryConfiguration"/> instance loaded from the ApplicationInsights.config file.
@@ -197,7 +226,6 @@
         /// </summary>
         public void Dispose()
         {
-            // TODO: Implement a Finalizer to dispose this class.
             this.Dispose(true);
             GC.SuppressFinalize(this);
         }
