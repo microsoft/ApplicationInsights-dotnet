e6559cd6ccb4f1969248c61de95913b24aea472c
mafletch <mafletch@microsoft.com>
2016-01-19 13:45:51 -0800

    Reorganizing solutions to be easier to understand

--- runFunctionalTests.cmd
+++ runFunctionalTests.cmd
@@ -5,5 +5,5 @@ CALL buildRelease.cmd
 
 set BuildRoot=%~dp0..\bin\release\
 
-CALL "%VS140COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /UseVsixExtensions:true "%BuildRoot%Web\FunctionalTests\FunctionalTests\Functional.dll" "%BuildRoot%PerformanceCollector\PerformanceCollector.Tests\FunctionalTests\PerfCollector.FunctionalTests.dll" "%BuildRoot%Web\DependencyCollector\FunctionalTests\FuncTest\FuncTest.dll" /logger:trx
+CALL "%VS140COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /UseVsixExtensions:true "%BuildRoot%Test\DependencyCollector\FunctionalTests\FuncTest\FuncTest.dll" "%BuildRoot%Test\PerformanceCollector\FunctionalTests\PerfCollector.FunctionalTests.dll" "%BuildRoot%Test\Web\FunctionalTests\FunctionalTests\Functional.dll" /logger:trx
 
