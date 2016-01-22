ddf4b928e70f74c6e54f6a58beb9e7c97db79c95
mafletch <mafletch@microsoft.com>
2016-01-21 16:25:02 -0800

    Added changes necessary for microbuild v2

---
+++ VSOPushNugetPackages.cmd
@@ -0,0 +1,25 @@
+setlocal
+
+if "%NUGET_KEY%"=="" SET NUGET_KEY=%1
+
+echo "key: '%NUGET_KEY%'"
+
+set BINROOT=%AGENT_BUILDDIRECTORY%\Release\NuGet
+if not exist %BINROOT% echo "Error: '%BINROOT%' does not exist."&goto :eof
+
+set NUGET=%BUILD_SOURCESDIRECTORY%\NuGet.exe
+if not exist %NUGET% echo "Error: '%NUGET%' does not exist."&goto :eof
+
+set NUGET_GALLERY=https://www.myget.org/F/applicationinsights/api/v2/package
+
+for /r "%BINROOT%" %%P in (*.nupkg) do call :push %%P
+goto :eof
+
+:push 
+set PACKAGE=%1
+if %PACKAGE:.symbols.=% == %PACKAGE% (
+    %NUGET% push "%PACKAGE%" %NUGET_KEY% -source %NUGET_GALLERY%
+)
+goto :eof
+
+endlocal
