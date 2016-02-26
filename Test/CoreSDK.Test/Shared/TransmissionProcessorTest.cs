9e087c6c773c20aff91d7955b9b58326c66a6ae8
Sergey Kanzhelev <SergKanz@microsoft.com>
2016-02-25 21:30:23 -0800

    fix for synchronization issue between debug output and channel

--- Test/CoreSDK.Test/Shared/TransmissionProcessorTest.cs
+++ Test/CoreSDK.Test/Shared/TransmissionProcessorTest.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-    
+
     using Microsoft.ApplicationInsights.Channel;
     using Microsoft.ApplicationInsights.DataContracts;
     using Microsoft.ApplicationInsights.Extensibility;
@@ -14,13 +14,15 @@
     using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 #endif
     using Assert = Xunit.Assert;
-    
+    using Extensibility.Implementation.Platform;
+    using System.Threading.Tasks;
+    using System.Linq;
+    using System.Threading;
     [TestClass]
     public class TransmissionProcessorTest
     {
-        
         #region Tests
-        
+
         [TestMethod]
         public void TransmissionProcessorTransmitsAllDataWhenNoOtherProcessorPresent()
         {
@@ -54,6 +56,53 @@
 
             var configuration = new TelemetryConfiguration { InstrumentationKey = "Test key", TelemetryChannel = null };
             Assert.Throws<InvalidOperationException>(() => new TransmissionProcessor(configuration).Process(new StubTelemetry()));
+        }
+
+        [TestMethod]
+        public void TransmissionProcessorStartsChannelSanitizationAfterDebugOutputSanitization()
+        {
+            var debugOutput = new StubDebugOutput
+            {
+                OnWriteLine = message =>
+                {
+                    // do nothing
+                },
+                OnIsAttached = () => true,
+            };
+
+            PlatformSingleton.Current = new StubPlatform { OnGetDebugOutput = () => debugOutput };
+
+            var channel = new StubTelemetryChannel { OnSend = t => new Task(() =>
+                 {
+                     ((ITelemetry)t).Sanitize();
+                 }).Start()
+            };
+            var configuration = new TelemetryConfiguration { InstrumentationKey = "Test key", TelemetryChannel = channel };
+
+            var client = new TelemetryClient(configuration);
+
+            var transmissionProcessor = new TransmissionProcessor(configuration);
+
+            const int ItemsToGenerate = 100;
+            Random random = new Random();
+
+            for (int i = 0; i < ItemsToGenerate; i++)
+            {
+                EventTelemetry telemetry = new EventTelemetry();
+
+                int len = random.Next(50);
+
+                for (int j = 0; j < len; j++)
+                {
+                    telemetry.Properties.Add(j.ToString(), j.ToString());
+                }
+
+                transmissionProcessor.Process(telemetry);
+            }
+
+            // There were a bug that causes Sanitize call from DebugOutput tracer conflict with Sanitize call from Channel
+            // If no exceptions here - everything fine
+            Assert.True(true);
         }
 
         #endregion       
