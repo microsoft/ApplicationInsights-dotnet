43ae15f43df782477794b5d14cbad649e14523c7
kartang <kartang@KARTANGRC2RE>
2016-04-22 16:06:41 -0700

    Refining MVC application by removing additional files and contents

--- test/MVCFramework45.FunctionalTests/wwwroot/_references.js
+++ test/MVCFramework45.FunctionalTests/wwwroot/_references.js
@@ -1,7 +1,1 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="js/site.js" />
-/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+﻿
