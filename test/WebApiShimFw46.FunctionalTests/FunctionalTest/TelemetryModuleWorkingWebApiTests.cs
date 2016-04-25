9140ac85d96df46127da50b735afe58452495875
kartang <kartang@KARTANGRC2RE>
2016-04-25 16:51:09 -0700

    Change in tfm from dnx451 to net451 in a test- cleanup

--- test/WebApiShimFw46.FunctionalTests/FunctionalTest/TelemetryModuleWorkingWebApiTests.cs
+++ test/WebApiShimFw46.FunctionalTests/FunctionalTest/TelemetryModuleWorkingWebApiTests.cs
@@ -11,7 +11,7 @@
     {
         private const string assemblyName = "WebApiShimFw46.FunctionalTests";
 
-#if dnx451
+#if net451
 
         [Fact]
         public void TestBasicDependencyPropertiesAfterRequestingBasicPage()
