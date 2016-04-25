c5612bc78264c59599c11d3bac5cdcfbdd12f50f
abaranch <abaranch@microsoft.com>
2016-04-25 14:29:38 -0700

    Add script that downloads SM

---
+++ DownloadSM.ps1
@@ -0,0 +1,21 @@
+﻿function Download-AgentMsi
+{
+	param (
+		[string] $msiPath
+	)
+
+	Write-Host "Start - Agent MSI download process."
+
+	$downloadLink = "http://go.microsoft.com/fwlink/?LinkID=522371&clcid=0x409"
+	
+	Write-Host "Downloading from $downloadLink"
+	$wc = New-Object System.Net.WebClient
+	$wc.DownloadFile($downloadLink, $msiPath)
+	
+	Write-Host "End - Agent MSI download process."
+}
+
+$agentMsi = "ApplicationInsightsAgent.msi"
+$msiPath = Join-Path "C:\" $agentMsi
+
+Download-AgentMsi $msiPath
