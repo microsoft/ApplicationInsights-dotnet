01e78bb4fa1990d06abd39cd7f52691f2b28c977
abaranch <abaranch@microsoft.com>
2016-10-07 13:50:33 -0700

    Remove ipv4 validation (ipv6 is supported, incorrect ip will be stripped off on the backend).

--- src/Core/Managed/Shared/Extensibility/Implementation/LocationContext.cs
+++ src/Core/Managed/Shared/Extensibility/Implementation/LocationContext.cs
@@ -1,12 +1,6 @@
 ﻿namespace Microsoft.ApplicationInsights.Extensibility.Implementation
 {    
-    using System;
-    using System.Collections;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-
-    using Microsoft.ApplicationInsights.DataContracts;
     using Microsoft.ApplicationInsights.Extensibility.Implementation.External;
 
     /// <summary>
@@ -33,51 +27,11 @@
 
             set 
             {
-                if (value != null && this.IsIpV4(value))
+                if (value != null)
                 {
                     this.tags.SetStringValueOrRemove(ContextTagKeys.Keys.LocationIp, value);
                 }
             }
-        }
-
-        private bool IsIpV4(string ip)
-        {
-            if ((ip.Length > 15) || (ip.Length < 7))
-            {
-                return false;
-            }
-
-            if (Enumerable.Any<char>(
-                    Enumerable.Cast<char>((IEnumerable)ip), 
-                    delegate(char c)
-                    {
-                        if ((c >= '0') && (c <= '9'))
-                        {
-                            return false;
-                        }
-
-                        return c != '.';
-                    }))
-            {
-                return false;
-            }
-
-            string[] strArray = ip.Split(new char[] { '.' });
-            if (strArray.Length != 4)
-            {
-                return false;
-            }
-
-            foreach (string str in strArray)
-            {
-                byte num;
-                if (!byte.TryParse(str, out num))
-                {
-                    return false;
-                }
-            }
-
-            return true;
         }
     }
 }
