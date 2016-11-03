75373e57dcf8d646c54ee188461c373f2cc98939
Karthik Tangirala <kartang@microsoft.com>
2016-11-03 12:23:00 -0700

    Change the proximity to 0.005 from 0.0001 in cpu percentage perf collection tests

--- Src/PerformanceCollector/Unit.Tests/WebAppPerformanceCollector/CPUPercenageGaugeTests.cs
+++ Src/PerformanceCollector/Unit.Tests/WebAppPerformanceCollector/CPUPercenageGaugeTests.cs
@@ -21,10 +21,9 @@
             Assert.IsTrue(Math.Abs(value1) < 0.000001);
 
             Thread.Sleep(TimeSpan.FromSeconds(10));
-
             double value2 = gauge.GetValueAndReset();
             Assert.IsTrue(
-                Math.Abs(value2 - ((24843750 - 24062500.0) / TimeSpan.FromSeconds(10).Ticks * 100.0)) < 0.0001, 
+                Math.Abs(value2 - ((24843750 - 24062500.0) / TimeSpan.FromSeconds(10).Ticks * 100.0)) < 0.005, 
                 string.Format(CultureInfo.InvariantCulture, "Actual: {0}, Expected: {1}", value2, (24843750 - 24062500.0) / TimeSpan.FromSeconds(10).Ticks));
         }
     }
