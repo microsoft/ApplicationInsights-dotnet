da6dad1683b9ebff6f638b9fc9a557d1073f70bb
Sergey Kanzhelev <SergKanz@microsoft.com>
2016-11-10 14:18:58 -0800

    added new telemetry module

---
+++ Src/WindowsServer/WindowsServer.Shared.Tests/FirstChanceExceptionStatisticsTelemetryModuleTest.cs
@@ -0,0 +1,117 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.ExceptionServices;
+
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.Web.TestFramework;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Assert = Xunit.Assert;
+
+    [TestClass]
+    public class FirstChanceExceptionStatisticsTelemetryModuleTest : IDisposable
+    {
+        private TelemetryConfiguration configuraiton;
+        private IList<ITelemetry> items;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.items = new List<ITelemetry>();
+
+            this.configuraiton = new TelemetryConfiguration();
+
+            this.configuraiton.TelemetryChannel = new StubTelemetryChannel
+            {
+                OnSend = telemetry => this.items.Add(telemetry),
+                EndpointAddress = "http://test.com"
+            };
+            this.configuraiton.InstrumentationKey = "MyKey";
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            this.configuraiton = null;
+            this.items.Clear();
+        }
+
+        [TestMethod]
+        public void FirstChanceExceptionStatisticsTelemetryModuleDoNotThrowOnNullException()
+        {
+            EventHandler<FirstChanceExceptionEventArgs> handler = null;
+            using (var module = new FirstChanceExceptionStatisticsTelemetryModule(
+                h => handler = h,
+                _ => { }))
+            {
+                module.Initialize(this.configuraiton);
+                handler.Invoke(null, new FirstChanceExceptionEventArgs(null));
+            }
+        }
+
+        [TestMethod]
+        public void FirstChanceExceptionStatisticsTelemetryModuleRecievesFirstChance()
+        {
+            using (var module = new FirstChanceExceptionStatisticsTelemetryModule())
+            {
+                module.Initialize(this.configuraiton);
+
+                try
+                {
+                    throw new Exception("test");
+                }
+                catch (Exception exc)
+                {
+                    Assert.NotNull(exc);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ModuleConstructorCallsRegister()
+        {
+            EventHandler<FirstChanceExceptionEventArgs> handler = null;
+            using (var module = new FirstChanceExceptionStatisticsTelemetryModule(
+                h => handler = h,
+                _ => { }))
+            {
+                module.Initialize(this.configuraiton);
+            }
+
+            Assert.NotNull(handler);
+        }
+
+        [TestMethod]
+        public void DisposeCallsUnregister()
+        {
+            EventHandler<FirstChanceExceptionEventArgs> handler = null;
+            using (var module = new FirstChanceExceptionStatisticsTelemetryModule(
+                _ => { },
+                h => handler = h))
+            {
+                module.Initialize(this.configuraiton);
+            }
+
+            Assert.NotNull(handler);
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.configuraiton != null)
+                {
+                    this.configuraiton.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Src/WindowsServer/WindowsServer.Shared/FirstChanceExceptionStatisticsTelemetryModule.cs
@@ -0,0 +1,129 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer
+{
+    using System;
+    using System.Diagnostics;
+    using System.Runtime.ExceptionServices;
+    using Extensibility.Implementation.Tracing;
+    using Implementation;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation;
+    using Microsoft.ApplicationInsights.Web.Implementation;
+
+    /// <summary>
+    /// The module subscribed to AppDomain.CurrentDomain.FirstChanceException to send exceptions statistics to ApplicationInsights.
+    /// </summary>
+    public sealed class FirstChanceExceptionStatisticsTelemetryModule : ITelemetryModule, IDisposable
+    {
+        private readonly Action<EventHandler<FirstChanceExceptionEventArgs>> registerAction;
+        private readonly Action<EventHandler<FirstChanceExceptionEventArgs>> unregisterAction;
+        private readonly object lockObject = new object();
+
+        private TelemetryClient telemetryClient;
+        private bool isInitialized = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstChanceExceptionStatisticsTelemetryModule" /> class.
+        /// </summary>
+        public FirstChanceExceptionStatisticsTelemetryModule() : this(
+            action => AppDomain.CurrentDomain.FirstChanceException += action,
+            action => AppDomain.CurrentDomain.FirstChanceException -= action)
+        {
+        }
+
+        internal FirstChanceExceptionStatisticsTelemetryModule(
+            Action<EventHandler<FirstChanceExceptionEventArgs>> registerAction,
+            Action<EventHandler<FirstChanceExceptionEventArgs>> unregisterAction)
+        {
+            this.registerAction = registerAction;
+            this.unregisterAction = unregisterAction;
+        }
+
+        /// <summary>
+        /// Initializes the telemetry module.
+        /// </summary>
+        /// <param name="configuration">Telemetry Configuration used for creating TelemetryClient for sending exception statistics to Application Insights.</param>
+        public void Initialize(TelemetryConfiguration configuration)
+        {
+            // Core SDK creates 1 instance of a module but calls Initialize multiple times
+            if (!this.isInitialized)
+            {
+                lock (this.lockObject)
+                {
+                    if (!this.isInitialized)
+                    {
+                        this.isInitialized = true;
+
+                        this.telemetryClient = new TelemetryClient(configuration);
+                        this.telemetryClient.Context.GetInternalContext().SdkVersion = SdkVersionUtils.GetSdkVersion("stat:");
+
+                        this.registerAction(this.CalculateStatistics);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposing TaskSchedulerOnUnobservedTaskException instance.
+        /// </summary>
+        public void Dispose()
+        {
+            this.unregisterAction(this.CalculateStatistics);
+        }
+
+        private void CalculateStatistics(object sender, FirstChanceExceptionEventArgs firstChanceExceptionArgs)
+        {
+            try
+            {
+                var exception = firstChanceExceptionArgs?.Exception;
+
+                if (exception == null)
+                {
+                    WindowsServerEventSource.Log.FirstChanceExceptionCallbackExeptionIsNull();
+                    return;
+                }
+
+                WindowsServerEventSource.Log.FirstChanceExceptionCallbackCalled();
+
+                var type = exception.GetType().FullName;
+
+                // obtaining the operation name. At this stage we have no intention to send this telemetry item
+                ExceptionTelemetry fakeTelemetry = new ExceptionTelemetry(exception);
+                this.telemetryClient.Initialize(fakeTelemetry);
+
+                var operation = fakeTelemetry.Context.Operation.Name;
+
+                // obtaining failing method name by walking 1 frame up the stack
+                var failingMethod = new StackFrame(1).GetMethod();
+                var method = failingMethod.DeclaringType.FullName + "." + failingMethod.Name;
+
+                this.TrackStatistcis(type, operation, method);
+            }
+            catch (Exception exc)
+            {
+                try
+                {
+                    WindowsServerEventSource.Log.FirstChanceExceptionCallbackException(exc.ToInvariantString());
+                }
+                catch (Exception)
+                {
+                    // this is absolutely critical to not throw out of this method
+                    // Otherwise it will affect the customer application behavior significantly
+                }
+            }
+        }
+
+        private void TrackStatistcis(string type, string operation, string method)
+        {
+            var metricTelemetry = new MetricTelemetry();
+
+            metricTelemetry.Value = 1;
+
+            metricTelemetry.Properties["type"] = type;
+            metricTelemetry.Properties["operation"] = operation;
+            metricTelemetry.Properties["method"] = method;
+
+            this.telemetryClient.TrackMetric(metricTelemetry);
+        }
+    }
+}
