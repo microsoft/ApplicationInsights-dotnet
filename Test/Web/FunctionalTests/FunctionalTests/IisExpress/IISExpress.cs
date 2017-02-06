1d4f0c6f3ec289f148fd4365adefff1d33a5c6d2
Cijo THomas <cithomas@microsoft.com>
2017-02-06 15:17:38 -0800

    Revert to old behaviour for IISExpress, but with increased timeout. This is unblock beta 2 release and needs proper investigation later.

--- Test/Web/FunctionalTests/FunctionalTests/IisExpress/IISExpress.cs
+++ Test/Web/FunctionalTests/FunctionalTests/IisExpress/IISExpress.cs
@@ -84,25 +84,17 @@
 
         public void Stop()
         {
-            const int processExitWaitTimeout = 10000;
+            const int processExitWaitTimeout = 15000;
 
-            Trace.TraceInformation("Killing iisexpress.exe: pid={0}", this.hostProcess.Id);
+            Trace.TraceInformation("Stopping iisexpress.exe: pid={0}", this.hostProcess.Id);
 
-            try
-            {
-                this.hostProcess.Kill();
-            }
-            catch(InvalidOperationException ex)
-            {
-                // The process has already exited most likely if this exception.
-                Trace.TraceWarning("Kill threw exception. {0}" , ex.Message);
-            }
+            SendStopMessageToProcess(this.hostProcess.Id);
 
-            Trace.TraceInformation("Kill invoked. Waiting for exit of iisexpress.exe: pid={0}", this.hostProcess.Id);
+            Trace.TraceInformation("Waiting for exit of iisexpress.exe: pid={0}", this.hostProcess.Id);
             if (false == this.hostProcess.WaitForExit(processExitWaitTimeout))
             {
                 Trace.TraceWarning("iisexpress.exe process hasn't exited during expected time, terminating!");
-                throw new Exception("IISExpress.exe process has not exited!");
+                this.hostProcess.Kill();
             }
             else
             {
