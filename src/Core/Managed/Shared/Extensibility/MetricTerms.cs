d6c824edd8a6f296608d4994553fbb696695968c
macrogreg <macrogreg@live.com>
2017-03-14 22:19:19 +0700

    Metrics: Metric Constants as structured constants bag.

--- src/Core/Managed/Shared/Extensibility/MetricTerms.cs
+++ src/Core/Managed/Shared/Extensibility/MetricTerms.cs
@@ -1,125 +1,65 @@
 ﻿using System;
 
-namespace Microsoft.ApplicationInsights.Extensibility.MetricTerms
+namespace Microsoft.ApplicationInsights.Extensibility
 {
-    internal static class Shared
+    internal static class MetricTerms
     {
-        public const string MetricPropertiesNamePrefix = "Microsoft.ApplicationInsights.Metrics";
-    }
-
-    namespace Extraction.ProcessedByExtractors
-    {
-        internal static class Moniker
+        private const string MetricPropertiesNamePrefix = "Microsoft.ApplicationInsights.Metrics";
+        public static class Extraction
         {
-            public const string Key = Shared.MetricPropertiesNamePrefix + ".Extraction.ProcessedByExtractors";
-        }
-    }
-
-    namespace Autocollection
-    {
-        internal static class Moniker
-        {
-            public const string Key = Shared.MetricPropertiesNamePrefix + ".MetricIsAutocollected";
-            public const string Value = "True";
-        }
-
-        namespace MetricNames
-        {
-            internal static class Request
+            public static class ProcessedByExtractors
             {
-                public const string Duration = "Server response time";
-            }
-
-            internal static class DependencyCall
-            {
-                public const string Duration = "Dependency duration";
+                public static class Moniker
+                {
+                    public const string Key = MetricPropertiesNamePrefix + ".Extraction.ProcessedByExtractors";
+                }
             }
         }
 
-        namespace Request
+        public static class Autocollection
         {
-            internal static class PropertyName
+            public static class Moniker
             {
-                public const string Success = "Success";
-            }
-        }
+                public const string Key = MetricPropertiesNamePrefix + ".MetricIsAutocollected";
+                public const string Value = "True";
 
-        namespace DependencyCall
-        {
-            internal static class PropertyName
-            {
-                public const string Success = "Success";
-                public const string TypeName = "Type";
             }
 
-            internal static class TypeName
+            public static class MetricNames
             {
-                public const string Other = "Other";
+                public static class Request
+                {
+                    public const string Duration = "Server response time";
+                }
+
+                public static class DependencyCall
+                {
+                    public const string Duration = "Dependency duration";
+                }
+            }
+
+            public static class Request
+            {
+                public static class PropertyName
+                {
+                    public const string Success = "Success";
+                }
+            }
+
+            public static class DependencyCall
+            {
+                public static class PropertyName
+                {
+                    public const string Success = "Success";
+                    public const string TypeName = "Type";
+                }
+
+                public static class TypeName
+                {
+                    public const string Other = "Other";
+                    public const string Unknown = "Unknown";
+                }
             }
         }
     }
 }
-
-
-//namespace Microsoft.ApplicationInsights.Extensibility
-//{
-//    internal static class MetricTerms
-//    {
-//        private const string MetricPropertiesNamePrefix = "Microsoft.ApplicationInsights.Metrics";
-//        public static class Extraction
-//        {
-//            public static class ProcessedByExtractors
-//            {
-//                public static class Moniker
-//                {
-//                    public const string Key = MetricPropertiesNamePrefix + ".Extraction.ProcessedByExtractors";
-//                }
-//            }
-//        }
-
-//        public static class Autocollection
-//        {
-//            public static class Moniker
-//            {
-//                public const string Key = MetricPropertiesNamePrefix + ".MetricIsAutocollected";
-//                public const string Value = "True";
-
-//            }
-
-//            public static class MetricNames
-//            {
-//                public static class Request
-//                {
-//                    public const string Duration = "Server response time";
-//                }
-
-//                public static class DependencyCall
-//                {
-//                    public const string Duration = "Dependency duration";
-//                }
-//            }
-
-//            public static class Request
-//            {
-//                public static class PropertyName
-//                {
-//                    public const string Success = "Success";
-//                }
-//            }
-
-//            public static class DependencyCall
-//            {
-//                public static class PropertyName
-//                {
-//                    public const string Success = "Success";
-//                    public const string TypeName = "Type";
-//                }
-
-//                public static class TypeName
-//                {
-//                    public const string Other = "Other";
-//                }
-//            }
-//        }
-//    }
-//}
