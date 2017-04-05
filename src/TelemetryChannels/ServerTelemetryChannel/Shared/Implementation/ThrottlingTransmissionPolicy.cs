de105c304b2e4274f738738493452dd168e8ff41
YuliaSafarova <iusafaro@microsoft.com>
2017-04-05 11:58:44 -0700

    SDK stops sending telemetry and does not start again. #480 (#513)
    * Make each policy having each own task timer
    
    * CurrentDelay initialization
    
    * StubTransmissionScheduledPolicy fix for unit tests
    
    * Fix indents

--- src/TelemetryChannels/ServerTelemetryChannel/Shared/Implementation/ThrottlingTransmissionPolicy.cs
+++ src/TelemetryChannels/ServerTelemetryChannel/Shared/Implementation/ThrottlingTransmissionPolicy.cs
@@ -10,9 +10,10 @@
     using TaskEx = System.Threading.Tasks.Task;
 #endif
 
-    internal class ThrottlingTransmissionPolicy : TransmissionPolicy
+    internal class ThrottlingTransmissionPolicy : TransmissionPolicy, IDisposable
     {
         private BackoffLogicManager backoffLogicManager;
+        private TaskTimerInternal pauseTimer = new TaskTimerInternal { Delay = TimeSpan.FromSeconds(BackoffLogicManager.SlotDelayInSeconds) };
 
         public override void Initialize(Transmitter transmitter)
         {
@@ -25,6 +26,12 @@
 
             base.Initialize(transmitter);
             transmitter.TransmissionSent += this.HandleTransmissionSentEvent;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         private void HandleTransmissionSentEvent(object sender, TransmissionProcessedEventArgs e)
@@ -54,13 +61,13 @@
                     this.backoffLogicManager.ReportBackoffEnabled((int)httpWebResponse.StatusCode);
                     this.Transmitter.Enqueue(e.Transmission);
 
-                    this.backoffLogicManager.ScheduleRestore(
-                        httpWebResponse.RetryAfterHeader, 
+                    this.pauseTimer.Delay = this.backoffLogicManager.GetBackOffTimeInterval(httpWebResponse.RetryAfterHeader);
+                    this.pauseTimer.Start(
                         () =>
-                            {
-                                this.ResetPolicy();
-                                return TaskEx.FromResult<object>(null);
-                            });
+                        {
+                            this.ResetPolicy();
+                            return TaskEx.FromResult<object>(null);
+                        });
                 }
             }
         }
@@ -72,6 +79,18 @@
             this.MaxStorageCapacity = null;
             this.LogCapacityChanged();
             this.Apply();     
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.pauseTimer != null)
+                {
+                    this.pauseTimer.Dispose();
+                    this.pauseTimer = null;
+                }
+            }
         }
     }
 }
