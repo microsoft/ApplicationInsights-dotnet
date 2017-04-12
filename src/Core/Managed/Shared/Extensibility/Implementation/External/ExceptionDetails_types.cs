270bdf8a6167d67406d2fb14fd4aa905905b8e92
yantang <yantang@microsoft.com>
2017-04-12 16:28:12 -0700

    Fix the issue that ExceptionDetails class doesn't decorate the EventData attribute correctly

--- src/Core/Managed/Shared/Extensibility/Implementation/External/ExceptionDetails_types.cs
+++ src/Core/Managed/Shared/Extensibility/Implementation/External/ExceptionDetails_types.cs
@@ -26,48 +26,48 @@
 
 namespace Microsoft.ApplicationInsights.Extensibility.Implementation.External
 {
-    using System.Collections.Concurrent;
+    using System;
     using System.Collections.Generic;
 
-    
-    
+
+
     [System.CodeDom.Compiler.GeneratedCode("gbc", "0.4.1.0")]
     internal partial class ExceptionDetails
     {
-        
-        
+
+
         public int id { get; set; }
 
-        
-        
+
+
         public int outerId { get; set; }
 
-        
-        
-        
+
+
+
         public string typeName { get; set; }
 
-        
-        
-        
+
+
+
         public string message { get; set; }
 
-        
-        
+
+
         public bool hasFullStack { get; set; }
 
-        
-        
-        
+
+
+
         public string stack { get; set; }
 
-        
-        
+
+
         public IList<StackFrame> parsedStack { get; set; }
 
         public ExceptionDetails()
             : this("AI.ExceptionDetails", "ExceptionDetails")
-        {}
+        { }
 
         protected ExceptionDetails(string fullName, string name)
         {
@@ -76,6 +76,31 @@ namespace Microsoft.ApplicationInsights.Extensibility.Implementation.External
             hasFullStack = true;
             stack = "";
             parsedStack = new List<StackFrame>();
+        }
+
+        /// <summary>
+        /// Creates a new instance of ExceptionDetails from a System.Exception and a parent ExceptionDetails.
+        /// </summary>
+        internal static ExceptionDetails CreateWithoutStackInfo(Exception exception, ExceptionDetails parentExceptionDetails)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException("exception");
+            }
+
+            var exceptionDetails = new External.ExceptionDetails()
+            {
+                id = exception.GetHashCode(),
+                typeName = exception.GetType().FullName,
+                message = exception.Message
+            };
+
+            if (parentExceptionDetails != null)
+            {
+                exceptionDetails.outerId = parentExceptionDetails.id;
+            }
+
+            return exceptionDetails;
         }
     }
 } // AI
