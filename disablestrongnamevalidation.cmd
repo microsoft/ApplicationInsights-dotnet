282707906f8c78a3119f1803a5533cce14ee6eda
Cijo Thomas <cithomas@microsoft.com>
2017-06-21 13:10:52 -0700

    Removed cmd

--- disablestrongnamevalidation.cmd
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe" -Vr *,31bf3856ad364e35
--- enablestrongnamevalidation.cmd
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe" -Vu *,31bf3856ad364e35
