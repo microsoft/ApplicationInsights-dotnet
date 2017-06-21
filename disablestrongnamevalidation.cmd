d7e2870f0f914983931818745777d6a040f57539
Cijo Thomas <cithomas@microsoft.com>
2017-06-21 10:28:59 -0700

    sn.exe appended

--- disablestrongnamevalidation.cmd
+++ disablestrongnamevalidation.cmd
@@ -1,2 +1,2 @@
-set SNEXE ="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools"
+set SNEXE ="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe"
 %SNEXE% -Vr *,31bf3856ad364e35
