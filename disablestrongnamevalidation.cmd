ff00b1bdf440c52e5284350f070c31fbc0a969fe
Cijo Thomas <cithomas@microsoft.com>
2017-06-21 10:08:10 -0700

    Added error logging

--- disablestrongnamevalidation.cmd
+++ disablestrongnamevalidation.cmd
@@ -1,2 +1,3 @@
 set VsDevCmd ="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+if not exist %VsDevCmd% echo "Error: '%VsDevCmd%' does not exist."
 %VsDevCmd% sn -Vr *,31bf3856ad364e35
