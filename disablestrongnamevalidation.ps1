86f0b2a325bd05e463dcd98b7b5b0e4c1ccb0316
Cijo Thomas <cithomas@microsoft.com>
2017-06-21 14:16:14 -0700

    moe logs

--- disablestrongnamevalidation.ps1
+++ disablestrongnamevalidation.ps1
@@ -1,2 +1,2 @@
-Get-ChildItem -Path "C:\Program Files (x86)\Microsoft SDKs\Windows"
+Get-ChildItem -Path "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A"
 & "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\sn.exe" -Vr *,31bf3856ad364e35
