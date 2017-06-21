5d4dcce33eafaa485edb43b02d71ef3dccaeba03
Cijo Thomas <cithomas@microsoft.com>
2017-06-21 10:29:52 -0700

    Tweeks

--- disablestrongnamevalidation.cmd
+++ disablestrongnamevalidation.cmd
@@ -1,2 +1,1 @@
-set SNEXE ="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe"
-%SNEXE% -Vr *,31bf3856ad364e35
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe" -Vr *,31bf3856ad364e35
--- enablestrongnamevalidation.cmd
+++ enablestrongnamevalidation.cmd
@@ -1,2 +1,1 @@
-set SNEXE ="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe"
-%SNEXE% -Vu *,31bf3856ad364e35
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\sn.exe" -Vu *,31bf3856ad364e35
