a4df76c14388a52f479a63ea5634266fd586ff31
Cijo Thomas <cithomas@microsoft.com>
2017-06-21 14:21:07 -0700

    Correct sn.exe path added.

--- disablestrongnamevalidation.ps1
+++ disablestrongnamevalidation.ps1
@@ -1,2 +1,1 @@
-Get-ChildItem -Path "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin" -Recurse
 & "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,31bf3856ad364e35
