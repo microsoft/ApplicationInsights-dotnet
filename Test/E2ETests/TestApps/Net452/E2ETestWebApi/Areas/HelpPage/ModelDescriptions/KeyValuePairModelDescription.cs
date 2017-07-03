e2fd459b37d70aea8d7acdf49fd6b6acd49a7255
Cijo Thomas <cithomas@microsoft.com>
2017-07-03 15:27:42 -0700

    Added default websites for E2E TestApps Added docker file Added new Project for E2E testing

---
+++ Test/E2ETests/TestApps/Net452/E2ETestWebApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace E2ETestWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
