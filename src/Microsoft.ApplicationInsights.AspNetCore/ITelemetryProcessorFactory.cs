3149b7fdf2e8a602a810cae6aca60391088ea583
Pavel Krymets <pavel@krymets.com>
2017-07-06 13:18:01 -0700

    Comments

--- src/Microsoft.ApplicationInsights.AspNetCore/ITelemetryProcessorFactory.cs
+++ src/Microsoft.ApplicationInsights.AspNetCore/ITelemetryProcessorFactory.cs
@@ -3,8 +3,15 @@
     using Microsoft.ApplicationInsights.Channel;
     using Microsoft.ApplicationInsights.Extensibility;
 
+    /// <summary>
+    /// Represents factory used to create <see cref="ITelemetryProcessor"/> with dependency injection support.
+    /// </summary>
     public interface ITelemetryProcessorFactory
     {
+        /// <summary>
+        /// Returns a <see cref="ITelemetryProcessor"/>,
+        /// given the next <see cref="ITelemetryProcessor"/> in the call chain.
+        /// </summary>
         ITelemetryProcessor Create(ITelemetryProcessor next);
     }
 }
