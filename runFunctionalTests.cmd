ada920d2299761684444078d8c3912501be5c8e0
Cijo Thomas <cithomas@microsoft.com>
2017-08-15 10:15:24 -0700

    Added netcore20 app and added tests for this app.

--- runFunctionalTests.cmd
+++ runFunctionalTests.cmd
@@ -1,10 +1,8 @@
 @echo off
 @setlocal enabledelayedexpansion enableextensions
 
-CALL buildRelease.cmd
-
-set BuildRoot=%~dp0..\bin\release\
+set BuildRoot=%~dp0..\bin\debug\
 set VSTestPath=%PROGRAMFILES(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 
-CALL "%VSTestPath%" /UseVsixExtensions:true "%BuildRoot%Test\DependencyCollector\FunctionalTests\FuncTest\FuncTest.dll" "%BuildRoot%Test\PerformanceCollector\FunctionalTests\PerfCollector.FunctionalTests.dll" "%BuildRoot%Test\Web\FunctionalTests\FunctionalTests\Functional.dll" /logger:trx
+CALL "%VSTestPath%" /UseVsixExtensions:true "%BuildRoot%Test\DependencyCollector\FunctionalTests\FuncTest\FuncTest.dll"  /TestCaseFilter:"TestCategory=.NET Core 2.0" /logger:trx
 
