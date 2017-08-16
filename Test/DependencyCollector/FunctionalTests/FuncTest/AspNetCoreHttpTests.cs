262bd4b85d1536e4c7d8985e559a6a552c653040
Cijo Thomas <cithomas@microsoft.com>
2017-08-16 12:05:35 -0700

    Removed tests logic with AI endpoint

--- Test/DependencyCollector/FunctionalTests/FuncTest/AspNetCoreHttpTests.cs
+++ Test/DependencyCollector/FunctionalTests/FuncTest/AspNetCoreHttpTests.cs
@@ -53,8 +53,7 @@
 
         [TestCleanup]
         public void MyTestCleanup()
-        {
-            Assert.IsFalse(DeploymentAndValidationTools.SdkEventListener.FailureDetected, "Failure is detected. Please read test output logs.");
+        {            
             DeploymentAndValidationTools.SdkEventListener.Stop();
         }               
 
