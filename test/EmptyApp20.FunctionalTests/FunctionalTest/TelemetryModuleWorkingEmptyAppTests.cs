4d91ca68aa7764536983a2edba89f7e0f2cc0583
Cijo Thomas <cithomas@microsoft.com>
2017-08-31 13:22:56 -0700

    Added EmptyApp20 and FunctionalTestUtils20

---
+++ test/EmptyApp20.FunctionalTests/FunctionalTest/TelemetryModuleWorkingEmptyAppTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EmptyApp20.FunctionalTests.FunctionalTest
+{
+    using FunctionalTestUtils;
+    using Xunit;
+
+    public class TelemetryModuleWorkingEmptyAppTests : TelemetryTestsBase
+    {
+        private const string assemblyName = "EmptyApp20.FunctionalTests";
+
+        // The NET451 conditional check is wrapped inside the test to make the tests visible in the test explorer. We can move them to the class level once if the issue is resolved.
+
+        [Fact]
+        public void TestBasicDependencyPropertiesAfterRequestingBasicPage()
+        {
+            this.ValidateBasicDependency(assemblyName, "/");
+        }
+
+        [Fact]
+        public void TestIfPerformanceCountersAreCollected()
+        {
+#if NET451 || NET46
+            ValidatePerformanceCountersAreCollected(assemblyName);
+#endif
+        }
+    }
+}
---
+++ test/FunctionalTestUtils20/InProcessServer.cs
@@ -0,0 +1,86 @@
+﻿namespace FunctionalTestUtils
+{
+    using System;
+    using System.IO;
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.AspNetCore;
+
+    // a variant of aspnet/Hosting/test/Microsoft.AspNetCore.Hosting.Tests/HostingEngineTests.cs
+    public class InProcessServer : IDisposable
+    {
+        private static Random random = new Random();
+
+        public static Func<IWebHostBuilder, IWebHostBuilder> UseApplicationInsights =
+            builder => builder.UseApplicationInsights();
+
+        private readonly Func<IWebHostBuilder, IWebHostBuilder> configureHost;
+        private IWebHost hostingEngine;
+        private string url;
+
+        private readonly BackTelemetryChannel backChannel;
+
+        public BackTelemetryChannel BackChannel
+        {
+            get
+            {
+                return this.backChannel;
+            }
+        }
+
+        public InProcessServer(string assemblyName, Func<IWebHostBuilder, IWebHostBuilder> configureHost = null)
+        {
+            this.configureHost = configureHost;
+            var machineName = Environment.GetEnvironmentVariable("COMPUTERNAME");
+            this.url = "http://" + machineName + ":" + random.Next(5000, 14000).ToString();
+            this.backChannel = this.Start(assemblyName);
+        }
+
+        public string BaseHost
+        {
+            get
+            {
+                return this.url;
+            }
+        }
+
+        public IServiceProvider ApplicationServices { get; private set; }
+
+        private BackTelemetryChannel Start(string assemblyName)
+        {
+
+#if NET46
+            var builder = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls(this.BaseHost)                
+                .UseKestrel()
+                .UseStartup(assemblyName)
+                .UseEnvironment("Production");
+#else
+
+            var builder = WebHost.CreateDefaultBuilder().UseStartup(assemblyName);
+#endif
+
+            if (configureHost != null)
+            {
+                builder = configureHost(builder);
+            }
+
+            this.hostingEngine = builder.Build();
+
+            this.hostingEngine.Start();
+
+            this.ApplicationServices = this.hostingEngine.Services;
+            return (BackTelemetryChannel)this.hostingEngine.Services.GetService<ITelemetryChannel>();
+        }
+
+        public void Dispose()
+        {
+            if (this.hostingEngine != null)
+            {
+                this.hostingEngine.Dispose();
+            }
+        }
+    }
+}
