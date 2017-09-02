604af43cbc4534724902e8091c420239b72de805
macrogreg <macrogreg@live.com>
2017-09-01 22:09:37 -0700

    Metrics: Dummy Test project builds and runs.

---
+++ Microsoft.ApplicationInsights.Metrics.Core.Tests/Microsoft.ApplicationInsights.Metrics/MultidimensionalCubeTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ApplicationInsights.Metrics;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class MultidimensionalCubeTests
+    {
+        [TestMethod]
+        public void DummyTest()
+        {
+           var cube = new MultidimensionalCube<int, int>( (dims) => 42, 5, 5, 5 );
+            Assert.IsNotNull(cube);
+        }
+    }
+}
