a8b6e57a557c269bb7e85e09d54ff499f1fa46a1
macrogreg <macrogreg@live.com>
2017-09-23 01:56:49 -0700

    Metrics: GrowingCollectionTests

---
+++ Microsoft.ApplicationInsights.Metrics.Core/InternalsVisibleTo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+//[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.Metrics.Core.Tests.Intrnl.dll")]
+[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.Metrics.Core.Tests.Intrnl")]
