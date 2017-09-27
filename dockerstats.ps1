baadd19e8a6ce79a3bd9309bb8bd863442974a34
Cijo Thomas <cithomas@microsoft.com>
2017-09-27 16:33:19 -0700

    cleanup step added in powershell to clean machines

--- dockerstats.ps1
+++ dockerstats.ps1
@@ -3,3 +3,7 @@
 & docker ps -a
 
 docker images -f "dangling=true" -q | ForEach-Object {docker rmi $_}
+& docker rmi -f e2etests_ingestionservice
+& docker rmi -f e2etests_e2etestwebapi
+& docker rmi -f e2etests_e2etestwebapp 
+
