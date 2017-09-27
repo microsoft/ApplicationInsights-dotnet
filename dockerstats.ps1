6da5c14048fc8e2363b75663273c7a897cf65ffd
Cijo Thomas <cithomas@microsoft.com>
2017-09-27 16:44:19 -0700

    Minor logs

--- dockerstats.ps1
+++ dockerstats.ps1
@@ -1,12 +1,19 @@
 & docker info
+
+Write-Host "Images before cleanup"
 & docker images
+
+Write-Host "Containers before cleanup"
 & docker ps -a
 
-docker images -f "dangling=true" -q | ForEach-Object {docker rmi $_}
 & docker rmi -f e2etests_ingestionservice
 & docker rmi -f e2etests_e2etestwebapi
 & docker rmi -f e2etests_e2etestwebapp 
 
+docker images -f "dangling=true" -q | ForEach-Object {docker rmi $_}
+
+Write-Host "Images after cleanup"
 & docker images
+Write-Host "Containers after cleanup"
 & docker ps -a
 
