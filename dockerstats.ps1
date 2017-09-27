4b4d1b5c37e27e9b693e3ccb5b3375fe3c7483d1
Cijo Thomas <cithomas@microsoft.com>
2017-09-27 16:49:13 -0700

    cleanup improvmemtns in powershell

--- dockerstats.ps1
+++ dockerstats.ps1
@@ -6,10 +6,22 @@ Write-Host "Images before cleanup"
 Write-Host "Containers before cleanup"
 & docker ps -a
 
+Write-Host "Stopping E2E Containers"
+& docker stop -f e2etests_ingestionservice
+& docker stop -f e2etests_e2etestwebapi
+& docker stop -f e2etests_e2etestwebapp 
+
+Write-Host "Removing E2E Containers"
+& docker rm e2etests_ingestionservice
+& docker rm e2etests_e2etestwebapi
+& docker rm e2etests_e2etestwebapp
+
+Write-Host "Removing E2E Images"
 & docker rmi -f e2etests_ingestionservice
 & docker rmi -f e2etests_e2etestwebapi
 & docker rmi -f e2etests_e2etestwebapp 
 
+Write-Host "Removing dangling images"
 docker images -f "dangling=true" -q | ForEach-Object {docker rmi $_}
 
 Write-Host "Images after cleanup"
