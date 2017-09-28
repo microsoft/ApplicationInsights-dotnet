5e2f9b4d879309ffe2fe9b1f6e46cfd966190be3
Cijo Thomas <cithomas@microsoft.com>
2017-09-28 11:23:46 -0700

    debug more

--- dockerstats.ps1
+++ dockerstats.ps1
@@ -32,6 +32,7 @@ Write-Host "Containers after cleanup"
 & docker ps -a
 
 Write-Host "Checking SQL"
+Install-Module -Name SqlServer
 Import-Module “sqlps” -DisableNameChecking
 $serverip = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" e2etests_sql-server_1
 Invoke-Sqlcmd -ServerInstance $serverip -Username sa -Password MSDNm4g4z!n3 -Database master -Query "SELECT name FROM master.dbo.sysdatabases"
