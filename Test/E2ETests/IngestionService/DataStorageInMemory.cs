627e2de58273f25941da784473a575a86c070620
Cijo Thomas <cithomas@microsoft.com>
2017-10-16 13:05:05 -0700

    Debug traces

--- Test/E2ETests/IngestionService/DataStorageInMemory.cs
+++ Test/E2ETests/IngestionService/DataStorageInMemory.cs
@@ -65,7 +65,7 @@
             {
                 foreach(var item in items)
                 {
-                    deletedItems.Add(item.ToString().Substring(0,5));
+                    deletedItems.Add(item.ToString().Substring(0,50));
                 }                
                 items.Clear();
             }
