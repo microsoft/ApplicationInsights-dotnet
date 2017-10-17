1f1ea43de63913600aa0371d2bfe380cf4d7b379
Derek Keeler <dekeeler@microsoft.com>
2017-10-17 12:04:43 -0700

    Add Framework version and ensure allowed properties is non-null
    - also remove public field misstep!

--- src/Microsoft.ApplicationInsights/Managed/Shared/Extensibility/Implementation/Tracing/HealthHeartbeatDefaultPayload.cs
+++ src/Microsoft.ApplicationInsights/Managed/Shared/Extensibility/Implementation/Tracing/HealthHeartbeatDefaultPayload.cs
@@ -10,7 +10,7 @@
         public const string FieldTargetFramework = "targetFramework";
         public const string FieldAppInsightsSdkVer = "appinsightsSdkVer";
 
-        public string EnabledProperties;
+        private string enabledProperties;
 
         public HealthHeartbeatDefaultPayload(string enabledProperties)
         {
@@ -22,6 +22,20 @@
         public int CurrentUnhealthyCount
         {
             get { return 0; }
+        }
+
+        public string EnabledProperties
+        {
+            get => this.enabledProperties;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("EnabledProperties", "Cannot set enabled properties to null or the empty string. Set the catch-all value '*' or some valid string to capture actual properties with.");
+                }
+
+                this.enabledProperties = value;
+            }
         }
 
         public IEnumerable<KeyValuePair<string, object>> GetPayloadProperties()
@@ -73,9 +87,21 @@
         {
 #if NET45
             return System.Environment.Version.ToString();
+#elif NETCORE
+            // this crazy slice of code came from here: https://github.com/dotnet/BenchmarkDotNet/issues/448
+            // I believe we will have more to do here, it would seem this is not something that the .NET Core makes easy
+
+            var assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
+            var assemblyPath = assembly.CodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+            int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
+            if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
+            {
+                return assemblyPath[netCoreAppIndex + 1];
+            }
+            return string.Empty;
 #else
-            return "na";
-            // TODO: How do we determine runtime framework in Net Core?
+            // is there any other framework we want to handle? 
+            return "unknown";
 #endif
         }
     }
