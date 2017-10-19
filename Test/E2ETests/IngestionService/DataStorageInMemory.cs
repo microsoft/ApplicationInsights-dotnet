3e9467941c7c6bb40c03cc26fd7336ea4db3dd1b
Cijo Thomas <cithomas@microsoft.com>
2017-10-18 17:58:56 -0700

    Delete endpoint returns few charsinstead of whole doc

--- Test/E2ETests/IngestionService/DataStorageInMemory.cs
+++ Test/E2ETests/IngestionService/DataStorageInMemory.cs
@@ -65,7 +65,7 @@
             {
                 foreach (var item in items)
                 {
-                    deletedItems.Add(item.ToString());
+                    deletedItems.Add(item.ToString().Substring(76, 124));
                 }
                 items.Clear();
             }
