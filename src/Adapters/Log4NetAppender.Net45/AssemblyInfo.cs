d4c3ca4ccd94b2026b3617d5be6bd2583130dc4b
Sergey Kanzhelev <SergKanz@microsoft.com>
2017-11-16 21:21:24 -0800

    1 of many... moving to the new projects types (#130)
    * TraceListener.Net45
    
    * mend
    
    * Log4NetAppender.Net45
    
    * ETW
    
    * add references
    
    * fix documentaiton files
    
    * fix PackageVersionFile
    
    * remove shared projects
    
    * fix

--- src/Adapters/Log4NetAppender.Net45/AssemblyInfo.cs
+++ src/Adapters/Log4NetAppender.Net45/AssemblyInfo.cs
@@ -14,9 +14,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.ApplicationInsights.NLogTarget")]
-[assembly: AssemblyDescription("Application Insights NLog Target is a custom target allowing you to send NLog log messages to Application Insights. Application Insights will collect your logs from multiple sources and provide rich powerful search capabilities. This functionality depends on Microsoft Monitoring Agent being installed onto your machine, http://go.microsoft.com/fwlink/?LinkID=328052.")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("Microsoft.ApplicationInsights.Log4NetAppender")]
+[assembly: AssemblyDescription("Application Insights Log4Net Appender is a customer appender allowing you to send Log4Net log messages to Application Insights. Application Insights will collect your logs from multiple sources and provide rich powerful search capabilities. This functionality depends on Microsoft Monitoring Agent being installed onto your machine, http://go.microsoft.com/fwlink/?LinkID=328052.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,9 +28,9 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44abc834-bd19-449f-ad64-f8c83ca078ed")]
+[assembly: Guid("599daa99-7d0b-4cae-81b8-3a73509f2efa")]
 
-[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.NLogTarget.Net45.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
+[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.Log4NetAppender.Net45.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
 
 internal static class AssemblyInfo
 {
--- src/Adapters/TraceListener.Net45/AssemblyInfo.cs
+++ src/Adapters/TraceListener.Net45/AssemblyInfo.cs
@@ -14,9 +14,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.ApplicationInsights.Log4NetAppender")]
-[assembly: AssemblyDescription("Application Insights Log4Net Appender is a customer appender allowing you to send Log4Net log messages to Application Insights. Application Insights will collect your logs from multiple sources and provide rich powerful search capabilities. This functionality depends on Microsoft Monitoring Agent being installed onto your machine, http://go.microsoft.com/fwlink/?LinkID=328052.")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("Microsoft.ApplicationInsights.TraceListener")]
+[assembly: AssemblyDescription("Application Insights Trace Listener is a customer trace listener allowing you to send trace log messages to Application Insights. Application Insights will collect your logs from multiple sources and provide rich powerful search capabilities. This functionality depends on Microsoft Monitoring Agent being installed onto your machine, http://go.microsoft.com/fwlink/?LinkID=328052.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,9 +28,9 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("599daa99-7d0b-4cae-81b8-3a73509f2efa")]
+[assembly: Guid("3aada9bf-fc15-42fd-ace5-4cb2f11c1f2a")]
 
-[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.Log4NetAppender.Net45.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
+[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.TraceListener.Net45.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
 
 internal static class AssemblyInfo
 {
