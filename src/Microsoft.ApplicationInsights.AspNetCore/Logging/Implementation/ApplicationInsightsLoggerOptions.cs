9cdec24a32e19e4e96c899b2a657025a4720e243
Rastislav Novotný <rasto@duracellko.net>
2017-12-12 00:44:07 +0100

    Add ApplicationInsightsLoggerOptions to configure, whether EventId data should be included in telemetry or not

---
+++ src/Microsoft.ApplicationInsights.AspNetCore/Logging/Implementation/ApplicationInsightsLoggerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.ApplicationInsights.AspNetCore.Logging
+{
+    /// <summary>
+    /// Options for configuration of <see cref="ApplicationInsightsLogger"/>.
+    /// </summary>
+    public class ApplicationInsightsLoggerOptions
+    {
+        /// <summary>
+        /// Gets or sets value indicating, whether EventId and EventName properties should be included in telemetry.
+        /// </summary>
+        public bool IncludeEventId { get; set; }
+    }
+}
--- src/Microsoft.ApplicationInsights.AspNetCore/Logging/Implementation/ApplicationInsightsLoggerProvider.cs
+++ src/Microsoft.ApplicationInsights.AspNetCore/Logging/Implementation/ApplicationInsightsLoggerProvider.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
 
     /// <summary>
     /// <see cref="ILoggerProvider"/> implementation that creates returns instances of <see cref="ApplicationInsightsLogger"/>
@@ -9,8 +10,11 @@
     [ProviderAlias("ApplicationInsights")]
     internal class ApplicationInsightsLoggerProvider : ILoggerProvider
     {
+        private static readonly Func<string, LogLevel, bool> trueFilter = (cat, level) => true;
+
         private readonly TelemetryClient telemetryClient;
         private readonly Func<string, LogLevel, bool> filter;
+        private readonly bool includeEventId;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ApplicationInsightsLoggerProvider"/> class.
@@ -21,10 +25,20 @@
             this.filter = filter;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationInsightsLoggerProvider"/> class.
+        /// </summary>
+        public ApplicationInsightsLoggerProvider(TelemetryClient telemetryClient, IOptions<ApplicationInsightsLoggerOptions> options)
+        {
+            this.telemetryClient = telemetryClient;
+            this.filter = trueFilter;
+            this.includeEventId = (options.Value?.IncludeEventId).GetValueOrDefault();
+        }
+
         /// <inheritdoc />
         public ILogger CreateLogger(string categoryName)
         {
-            return new ApplicationInsightsLogger(categoryName, this.telemetryClient, filter);
+            return new ApplicationInsightsLogger(categoryName, this.telemetryClient, filter, includeEventId);
         }
 
         /// <inheritdoc />
