29133bc039d926143903ae8cc4a10e2238765ba5
Derek Keeler <dekeeler@microsoft.com>
2018-02-28 16:11:22 -0800

    Change from using a TelemetryInitializer to a TelemetryModule

--- Src/WindowsServer/WindowsServer.Shared/AzureInstanceMetadataTelemetryModule.cs
+++ Src/WindowsServer/WindowsServer.Shared/AzureInstanceMetadataTelemetryModule.cs
@@ -1,27 +1,25 @@
 ﻿namespace Microsoft.ApplicationInsights.WindowsServer
 {
     using System.Threading.Tasks;
-    using Microsoft.ApplicationInsights.Channel;
     using Microsoft.ApplicationInsights.Extensibility;
     using Microsoft.ApplicationInsights.Extensibility.Implementation;
     using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing;
     using Microsoft.ApplicationInsights.WindowsServer.Implementation;
 
     /// <summary>
-    /// A telemetry initializer that adds Azure specific context information to the heartbeat, if it is available.
+    /// A telemetry module that adds Azure instance metadata context information to the heartbeat, if it is available.
     /// </summary>
-    public class AzureInstanceMetadataTelemetryInitializer : ITelemetryInitializer
+    public class AzureInstanceMetadataTelemetryModule : ITelemetryModule
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="AzureInstanceMetadataTelemetryInitializer" /> class.
+        /// Initializes a new instance of the <see cref="AzureInstanceMetadataTelemetryModule" /> class.
         /// Creates a heartbeat property collector that obtains and inserts data from the Azure Instance
         /// Metadata service if it is present and available to the currently running process. If it is not
         /// present no added IMS data is added to the heartbeat.
         /// </summary>
-        public AzureInstanceMetadataTelemetryInitializer()
+        /// <param name="unused">Unused parameter for this TelemetryModule.</param>
+        public void Initialize(TelemetryConfiguration unused)
         {
-            WindowsServerEventSource.Log.TelemetryInitializerLoaded(this.GetType().FullName);
-
             var telemetryModules = TelemetryModules.Instance;
             foreach (var module in telemetryModules.Modules)
             {
@@ -32,19 +30,9 @@
                     var heartbeatProperties = new AzureComputeMetadataHeartbeatPropertyProvider();
                     Task.Factory.StartNew(
                         async () => await heartbeatProperties.SetDefaultPayload(hbeatManager)
-                        .ConfigureAwait(false));                    
+                        .ConfigureAwait(false));
                 }
             }
-        }
-
-        /// <summary>
-        /// For this initializer nothing further needs to be done, as all the properties are cached within the heartbeat 
-        /// provider itself.
-        /// </summary>
-        /// <param name="telemetry">The telemetry to initialize (unused in this instance).</param>
-        public void Initialize(ITelemetry telemetry)
-        {
-            return;
         }
     }
 }
