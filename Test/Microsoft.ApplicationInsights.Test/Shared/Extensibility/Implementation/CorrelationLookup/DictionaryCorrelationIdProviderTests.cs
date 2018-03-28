7696181b1ccd106c9b11efb10ef0b11c55e4fe1d
Timothy Mothra Lee <tilee@microsoft.com>
2018-03-28 16:23:26 -0700

    removed dependency on CorrelationIdHelper

--- Test/Microsoft.ApplicationInsights.Test/Shared/Extensibility/Implementation/CorrelationLookup/DictionaryCorrelationIdProviderTests.cs
+++ Test/Microsoft.ApplicationInsights.Test/Shared/Extensibility/Implementation/CorrelationLookup/DictionaryCorrelationIdProviderTests.cs
@@ -10,8 +10,6 @@
         const string testIkey2 = nameof(testIkey2);
         const string testAppId1 = nameof(testAppId1);
         const string testAppId2 = nameof(testAppId2);
-        readonly string testCorrelationId1 = CorrelationIdHelper.FormatApplicationId(testAppId1);
-        readonly string testCorrelationId2 = CorrelationIdHelper.FormatApplicationId(testAppId2);
         ICorrelationIdProvider correlationIdProvider;
 
         [TestInitialize]
@@ -21,8 +19,8 @@
             {
                 DefinedIds = new Dictionary<string, string>
                 {
-                    {testIkey1, testCorrelationId1 },
-                    {testIkey2, testCorrelationId2 }
+                    {testIkey1, testAppId1 },
+                    {testIkey2, testAppId2 }
                 }
             };
         }
@@ -31,10 +29,10 @@
         public void VerifyLookupsAsExcepected()
         {
             Assert.IsTrue(correlationIdProvider.TryGetCorrelationId(testIkey1, out string actual1));
-            Assert.AreEqual(testCorrelationId1, actual1);
+            Assert.AreEqual(testAppId1, actual1);
 
             Assert.IsTrue(correlationIdProvider.TryGetCorrelationId(testIkey2, out string actual2));
-            Assert.AreEqual(testCorrelationId2, actual2);
+            Assert.AreEqual(testAppId2, actual2);
 
             Assert.IsFalse(correlationIdProvider.TryGetCorrelationId("abc", out string actual3));
             Assert.IsNull(actual3);
