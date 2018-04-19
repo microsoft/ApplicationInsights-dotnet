50427203f498e3046d8c7cc2b93b9f530bcf1b89
Derek Keeler <dekeeler@microsoft.com>
2018-04-19 13:51:03 -0700

    Shorten hearbeat internal sdk id to minimum necessary length

--- src/Microsoft.ApplicationInsights/Managed/Shared/Extensibility/Implementation/Tracing/HeartbeatProvider.cs
+++ src/Microsoft.ApplicationInsights/Managed/Shared/Extensibility/Implementation/Tracing/HeartbeatProvider.cs
@@ -29,7 +29,7 @@
         /// <summary>
         /// Value for property indicating 'app insights version' related specifically to heartbeats.
         /// </summary>
-        private static string sdkVersionPropertyValue = SdkVersionUtils.GetSdkVersion("hbeat:");
+        private static string sdkVersionPropertyValue = SdkVersionUtils.GetSdkVersion("hb:");
 
         /// <summary>
         /// The name of the heartbeat metric item and operation context. 
