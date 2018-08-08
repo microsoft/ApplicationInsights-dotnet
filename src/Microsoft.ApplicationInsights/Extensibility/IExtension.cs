9a8bd60653aa2cf97e5723dd758b97901d7cf052
Cijo Thomas <cithomas@microsoft.com>
2018-08-08 16:31:03 -0700

    Removed ICloneable as clone can be defined in IExtension itelf.

--- src/Microsoft.ApplicationInsights/Extensibility/IExtension.cs
+++ src/Microsoft.ApplicationInsights/Extensibility/IExtension.cs
@@ -3,7 +3,11 @@
     /// <summary>
     /// Interface for defining strongly typed extensions to telemetry types.
     /// </summary>
-    public interface IExtension : ISerializableWithWriter, ICloneable
+    public interface IExtension : ISerializableWithWriter
     {
+        /// <summary>
+        /// Deep clones the members of the class.
+        /// </summary>
+        IExtension DeepClone();
     }
 }
