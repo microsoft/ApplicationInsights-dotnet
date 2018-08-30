b8e465bdf03cad4133614b3c8833a71ad9bf9d17
MS-TimothyMothra <tilee@microsoft.com>
2018-08-30 16:13:46 -0700

    fix fxcop warning (#900)
    * fix fxcop tostring

--- src/ServerTelemetryChannel/Implementation/TransmissionStorage.cs
+++ src/ServerTelemetryChannel/Implementation/TransmissionStorage.cs
@@ -182,7 +182,11 @@
 
         private static string GetUniqueFileName(string extension)
         {
+#if NETSTANDARD1_3
             string fileName = Guid.NewGuid().ToString("N");
+#else
+            string fileName = Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture);
+#endif
             return Path.ChangeExtension(fileName, extension);
         }
 
