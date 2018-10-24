b5dfd109ec80f64bb379d2b4ae28956823bf36f5
shibayan <me@shibayan.jp>
2018-10-24 16:45:16 +0900

    Implemented Azure Search API parser

--- Src/DependencyCollector/Shared.Tests/Implementation/HttpParsers/AzureSearchHttpParserTests.cs
+++ Src/DependencyCollector/Shared.Tests/Implementation/HttpParsers/AzureSearchHttpParserTests.cs
@@ -13,47 +13,66 @@
         [TestMethod]
         public void AzureSearchHttpParserConvertsValidDependencies()
         {
-            var testCases = new List<Tuple<string, string, string>>
+            Dictionary<string, string> defaultProperties = null;
+            var indexProperties = new Dictionary<string, string> { ["Index"] = "myindex" };
+            var datasourceProperties = new Dictionary<string, string> { ["Data Source"] = "mydatasource" };
+            var indexerProperties = new Dictionary<string, string> { ["Indexer"] = "myindexer" };
+            var skillsetProperties = new Dictionary<string, string> { ["Skillset"] = "myskillset" };
+            var synonymProperties = new Dictionary<string, string> { ["Synonymmap"] = "mysynonym" };
+
+            var testCases = new List<Tuple<string, string, string, Dictionary<string, string>>>
             {
                 // Index operations
-                Tuple.Create("Create index", "POST", "https://myaccount.search.windows.net/indexes?api-version=2017-11-11"),
-                Tuple.Create("Update index", "PUT", "https://myaccount.search.windows.net/indexes/myindex?api-version=2017-11-11"),
-                Tuple.Create("List indexes", "GET", "https://myaccount.search.windows.net/indexes?api-version=2017-11-11  "),
-                Tuple.Create("Get index", "GET", "https://myaccount.search.windows.net/indexes/myindex?api-version=2017-11-11"),
-                Tuple.Create("Delete index", "DELETE", "https://myaccount.search.windows.net/indexes/myindex?api-version=2017-11-11"),
-                Tuple.Create("Get index statistics", "GET", "https://myaccount.search.windows.net/indexes/myindex/stats?api-version=2017-11-11"),
-                Tuple.Create("Analyze text", "POST", "https://myaccount.search.windows.net/indexes/myindex/analyze?api-version=2017-11-11"),
+                Tuple.Create("Create index", "POST", "https://myaccount.search.windows.net/indexes?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("Update index", "PUT", "https://myaccount.search.windows.net/indexes/myindex?api-version=2017-11-11", indexProperties),
+                Tuple.Create("List indexes", "GET", "https://myaccount.search.windows.net/indexes?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("Get index", "GET", "https://myaccount.search.windows.net/indexes/myindex?api-version=2017-11-11", indexProperties),
+                Tuple.Create("Delete index", "DELETE", "https://myaccount.search.windows.net/indexes/myindex?api-version=2017-11-11", indexProperties),
+                Tuple.Create("Get index statistics", "GET", "https://myaccount.search.windows.net/indexes/myindex/stats?api-version=2017-11-11", indexProperties),
+                Tuple.Create("Analyze text", "POST", "https://myaccount.search.windows.net/indexes/myindex/analyze?api-version=2017-11-11", indexProperties),
 
                 // Document operations
-                Tuple.Create("Add/update/delete documents", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/index?api-version=2017-11-11"),
-                Tuple.Create("Search documents", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs?[query parameters]"),
-                Tuple.Create("Search documents", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/search?api-version=2017-11-11"),
-                Tuple.Create("Suggestions", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/suggest?[query parameters]"),
-                Tuple.Create("Suggestions", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/suggest?api-version=2017-11-11"),
-                Tuple.Create("Autocomplete", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/autocomplete?[query parameters]"),
-                Tuple.Create("Autocomplete", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/autocomplete?api-version=2017-11-11"),
-                Tuple.Create("Lookup document", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/key?[query parameters]"),
-                Tuple.Create("Count documents", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/$count?api-version=2017-11-11"),
+                Tuple.Create("Add/update/delete documents", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/index?api-version=2017-11-11", indexProperties),
+                Tuple.Create("Search documents", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs?search=abc", indexProperties),
+                Tuple.Create("Search documents", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/search?api-version=2017-11-11", indexProperties),
+                Tuple.Create("Suggestions", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/suggest?search=abc", indexProperties),
+                Tuple.Create("Suggestions", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/suggest?api-version=2017-11-11", indexProperties),
+                Tuple.Create("Autocomplete", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/autocomplete?search=abc", indexProperties),
+                Tuple.Create("Autocomplete", "POST", "https://myaccount.search.windows.net/indexes/myindex/docs/autocomplete?api-version=2017-11-11", indexProperties),
+                Tuple.Create("Lookup document", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/key?$select=*", indexProperties),
+                Tuple.Create("Count documents", "GET", "https://myaccount.search.windows.net/indexes/myindex/docs/$count?api-version=2017-11-11", indexProperties),
 
                 // Indexer operations
-                Tuple.Create("Create data source", "POST", "https://myaccount.search.windows.net/datasources?api-version=2017-11-11"),
+                Tuple.Create("Create data source", "POST", "https://myaccount.search.windows.net/datasources?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("Create indexer", "POST", "https://myaccount.search.windows.net/indexers?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("Delete data source", "DELETE", "https://myaccount.search.windows.net/datasources/mydatasource?api-version=2017-11-11", datasourceProperties),
+                Tuple.Create("Delete indexer", "DELETE", "https://myaccount.search.windows.net/indexers/myindexer?api-version=2017-11-11", indexerProperties),
+                Tuple.Create("Get data source", "GET", "https://myaccount.search.windows.net/datasources/mydatasource?api-version=2017-11-11", datasourceProperties),
+                Tuple.Create("Get indexer", "GET", "https://myaccount.search.windows.net/indexers/myindexer?api-version=2017-11-11", indexerProperties),
+                Tuple.Create("Get indexer status", "GET", "https://myaccount.search.windows.net/indexers/myindexer/status?api-version=2017-11-11", indexerProperties),
+                Tuple.Create("List data sources", "GET", "https://myaccount.search.windows.net/datasources?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("List indexers", "GET", "https://myaccount.search.windows.net/indexers?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("Reset indexer", "POST", "https://myaccount.search.windows.net/indexers/myindexer/reset?api-version=2017-11-11", indexerProperties),
+                Tuple.Create("Run indexer", "POST", "https://myaccount.search.windows.net/indexers/myindexer/run?api-version=2017-11-11", indexerProperties),
+                Tuple.Create("Update data source", "PUT", "https://myaccount.search.windows.net/datasources/mydatasource?api-version=2017-11-11", datasourceProperties),
+                Tuple.Create("Update indexer", "PUT", "https://myaccount.search.windows.net/indexers/myindexer?api-version=2017-11-11", indexerProperties),
 
                 // Service operations
-                Tuple.Create("Get service statistics", "GET", "https://myaccount.search.windows.net/servicestats?api-version=2017-11-11"),
+                Tuple.Create("Get service statistics", "GET", "https://myaccount.search.windows.net/servicestats?api-version=2017-11-11", defaultProperties),
 
                 // Skillset operations
-                Tuple.Create("Create skillset", "PUT", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview"),
-                Tuple.Create("Delete skillset", "DELETE", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview"),
-                Tuple.Create("Get skillset", "GET", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview"),
-                Tuple.Create("List skillsets", "GET", "https://myaccount.search.windows.net/skillsets?api-version=2017-11-11-Preview"),
-                Tuple.Create("Update skillset", "PUT", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview"),
+                Tuple.Create("Create skillset", "POST", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview", skillsetProperties),
+                Tuple.Create("Delete skillset", "DELETE", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview", skillsetProperties),
+                Tuple.Create("Get skillset", "GET", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview", skillsetProperties),
+                Tuple.Create("List skillsets", "GET", "https://myaccount.search.windows.net/skillsets?api-version=2017-11-11-Preview", defaultProperties),
+                Tuple.Create("Update skillset", "PUT", "https://myaccount.search.windows.net/skillsets/myskillset?api-version=2017-11-11-Preview", skillsetProperties),
 
-                // Synonyms operations
-                Tuple.Create("Create synonym map", "POST", "https://myaccount.search.windows.net/synonymmaps?api-version=2017-11-11"),
-                Tuple.Create("Update synonym map", "PUT", "https://myaccount.search.windows.net/synonymmaps/mysynonym?api-version=2017-11-11"),
-                Tuple.Create("List synonym maps", "GET", "https://myaccount.search.windows.net/synonymmaps?api-version=2017-11-11"),
-                Tuple.Create("Get synonym map", "GET", "https://myaccount.search.windows.net/synonymmaps/mysynonym?api-version=2017-11-11"),
-                Tuple.Create("Delete synonym map", "DELETE", "https://myaccount.search.windows.net/synonymmaps/mysynonym?api-version=2017-11-11")
+                // Synonym operations
+                Tuple.Create("Create synonym map", "POST", "https://myaccount.search.windows.net/synonymmaps?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("Update synonym map", "PUT", "https://myaccount.search.windows.net/synonymmaps/mysynonym?api-version=2017-11-11", synonymProperties),
+                Tuple.Create("List synonym maps", "GET", "https://myaccount.search.windows.net/synonymmaps?api-version=2017-11-11", defaultProperties),
+                Tuple.Create("Get synonym map", "GET", "https://myaccount.search.windows.net/synonymmaps/mysynonym?api-version=2017-11-11", synonymProperties),
+                Tuple.Create("Delete synonym map", "DELETE", "https://myaccount.search.windows.net/synonymmaps/mysynonym?api-version=2017-11-11", synonymProperties)
             };
 
             foreach (var testCase in testCases)
@@ -61,14 +80,16 @@
                 this.AzureSearchHttpParserConvertsValidDependencies(
                     testCase.Item1,
                     testCase.Item2,
-                    testCase.Item3);
+                    testCase.Item3,
+                    testCase.Item4);
             }
         }
 
         private void AzureSearchHttpParserConvertsValidDependencies(
             string operation,
             string verb,
-            string url)
+            string url,
+            Dictionary<string, string> properties)
         {
             var parsedUrl = new Uri(url);
 
@@ -86,6 +107,15 @@
             Assert.AreEqual(parsedUrl.Host, d.Target, operation);
             Assert.AreEqual(operation, d.Name, operation);
 
+            if (properties != null)
+            {
+                foreach (var property in properties)
+                {
+                    Assert.IsTrue(d.Properties.TryGetValue(property.Key, out var value), operation);
+                    Assert.AreEqual(property.Value, value, operation);
+                }
+            }
+
             // Parse without verb
             d = new DependencyTelemetry(
                 dependencyTypeName: RemoteDependencyConstants.HTTP,
@@ -98,8 +128,17 @@
             Assert.IsTrue(success, operation);
             Assert.AreEqual(RemoteDependencyConstants.AzureSearch, d.Type, operation);
             Assert.AreEqual(parsedUrl.Host, d.Target, operation);
-            string moniker = HttpParsingHelper.BuildOperationMoniker(null, HttpParsingHelper.ParseResourcePath(parsedUrl.AbsolutePath));
+            string moniker = AzureSearchHttpParser.BuildOperationMoniker(null, HttpParsingHelper.ParseResourcePath(parsedUrl.AbsolutePath));
             Assert.AreEqual(moniker, d.Name, operation);
+
+            if (properties != null)
+            {
+                foreach (var property in properties)
+                {
+                    Assert.IsTrue(d.Properties.TryGetValue(property.Key, out var value), operation);
+                    Assert.AreEqual(property.Value, value, operation);
+                }
+            }
         }
     }
 }
--- Src/DependencyCollector/Shared/Implementation/HttpParsers/AzureSearchHttpParser.cs
+++ Src/DependencyCollector/Shared/Implementation/HttpParsers/AzureSearchHttpParser.cs
@@ -26,9 +26,54 @@
             ["POST /indexes/*/analyze"] = "Analyze text",
 
             // Document operations
-            ["POST /indexes"] = "Add/update/delete documents",
+            ["POST /indexes/*/docs/index"] = "Add/update/delete documents",
+            ["GET /indexes/*/docs"] = "Search documents",
+            ["POST /indexes/*/docs/search"] = "Search documents",
+            ["GET /indexes/*/docs/suggest"] = "Suggestions",
+            ["POST /indexes/*/docs/suggest"] = "Suggestions",
+            ["GET /indexes/*/docs/autocomplete"] = "Autocomplete",
+            ["POST /indexes/*/docs/autocomplete"] = "Autocomplete",
+            ["GET /indexes/*/docs/key"] = "Lookup document",
+            ["GET /indexes/*/docs/$count"] = "Count documents",
+
+            // Indexer operations
+            ["POST /datasources"] = "Create data source",
+            ["POST /indexers"] = "Create indexer",
+            ["DELETE /datasources/*"] = "Delete data source",
+            ["DELETE /indexers/*"] = "Delete indexer",
+            ["GET /datasources/*"] = "Get data source",
+            ["GET /indexers/*"] = "Get indexer",
+            ["GET /indexers/*/status"] = "Get indexer status",
+            ["GET /datasources"] = "List data sources",
+            ["GET /indexers"] = "List indexers",
+            ["POST /indexers/*/reset"] = "Reset indexer",
+            ["POST /indexers/*/run"] = "Run indexer",
+            ["PUT /datasources/*"] = "Update data source",
+            ["PUT /indexers/*"] = "Update indexer",
+
+            // Service operations
+            ["GET /servicestats"] = "Get service statistics",
+
+            // Skillset operations
+            ["POST /skillsets/*"] = "Create skillset",
+            ["DELETE /skillsets/*"] = "Delete skillset",
+            ["GET /skillsets/*"] = "Get skillset",
+            ["GET /skillsets"] = "List skillsets",
+            ["PUT /skillsets/*"] = "Update skillset",
+
+            // Synonym operations
+            ["POST /synonymmaps"] = "Create synonym map",
+            ["PUT /synonymmaps/*"] = "Update synonym map",
+            ["GET /synonymmaps"] = "List synonym maps",
+            ["GET /synonymmaps/*"] = "Get synonym map",
+            ["DELETE /synonymmaps/*"] = "Delete synonym map"
         };
 
+        /// <summary>
+        /// Tries parsing given dependency telemetry item. 
+        /// </summary>
+        /// <param name="httpDependency">Dependency item to parse. It is expected to be of HTTP type.</param>
+        /// <returns><code>true</code> if successfully parsed dependency.</returns>
         internal static bool TryParse(ref DependencyTelemetry httpDependency)
         {
             var name = httpDependency.Name;
@@ -49,22 +94,65 @@
             //// Azure Search REST API: https://docs.microsoft.com/en-us/rest/api/searchservice/
             ////
 
-            var account = host.Substring(0, host.IndexOf('.'));
-
             HttpParsingHelper.ExtractVerb(name, out var verb, out var nameWithoutVerb, AzureSearchSupportedVerbs);
 
             var resourcePath = HttpParsingHelper.ParseResourcePath(nameWithoutVerb);
 
-            var operation = HttpParsingHelper.BuildOperationMoniker(verb, resourcePath);
-            var operationName = GetOperationName(httpDependency, operation);
+            // populate properties
+            foreach (var resource in resourcePath)
+            {
+                if (resource.Value != null)
+                {
+                    var propertyName = GetPropertyNameForResource(resource.Key);
+                    if (propertyName != null)
+                    {
+                        httpDependency.Properties[propertyName] = resource.Value;
+                    }
+                }
+            }
+
+            var operation = BuildOperationMoniker(verb, resourcePath);
+            var operationName = GetOperationName(operation);
 
             httpDependency.Type = RemoteDependencyConstants.AzureSearch;
-            httpDependency.Name = operationName;
+            httpDependency.Name = string.IsNullOrEmpty(operationName) ? httpDependency.Target : operationName;
 
             return true;
         }
 
-        private static string GetOperationName(DependencyTelemetry httpDependency, string operation)
+        internal static string BuildOperationMoniker(string verb, List<KeyValuePair<string, string>> resourcePath)
+        {
+            var operation = HttpParsingHelper.BuildOperationMoniker(verb, resourcePath);
+
+            // Do not generate asterisk for docs path
+            if (resourcePath.Count > 1 && resourcePath[1].Key == "docs" && resourcePath[1].Value != null)
+            {
+                operation = operation.Remove(operation.Length - 1) + resourcePath[1].Value;
+            }
+
+            return operation;
+        }
+
+        private static string GetPropertyNameForResource(string resourceType)
+        {
+            switch (resourceType)
+            {
+                case "indexes":
+                    return "Index";
+                case "datasources":
+                    return "Data Source";
+                case "indexers":
+                    return "Indexer";
+                case "skillsets":
+                    return "Skillset";
+                case "synonymmaps":
+                    return "Synonymmap";
+                default:
+                    return null;
+            }
+        }
+
+        private static string GetOperationName(string operation)
         {
             if (!OperationNames.TryGetValue(operation, out var operationName))
             {
