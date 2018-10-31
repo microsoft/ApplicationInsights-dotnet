e98ec7a65f62596ea2356f101cdd53ac0b7c3552
Cijo Thomas <cithomas@microsoft.com>
2018-10-31 08:45:32 -0700

    try azuredep parser

--- Test/PerfTest/Program.cs
+++ Test/PerfTest/Program.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DependencyCollector;
 
 namespace PerfTest
 {
@@ -25,8 +26,10 @@ namespace PerfTest
             var builder = activeConfiguration.DefaultTelemetrySink.TelemetryProcessorChainBuilder;
             builder.Use((next) => { return new AutocollectedMetricsExtractor(next); });
             builder.UseSampling(5, excludedTypes: "Event");
-            builder.UseSampling(5, includedTypes: "Event");
+            builder.UseSampling(5, includedTypes: "Event");            
             builder.Build();
+
+            activeConfiguration.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
 
             var telemetryClient = new TelemetryClient(activeConfiguration);
 
@@ -50,7 +53,7 @@ namespace PerfTest
                     {
                         for (int j = 0; i < 50000; i++)
                         {
-                            telemetryClient.TrackDependency("SQL", "MySqlDb", "Name", "Select name from details",
+                            telemetryClient.TrackDependency("Http", "myaccount.blob.core.windows.net", "Name", "https://myaccount.blob.core.windows.net/my/container/myblob",
                                 DateTimeOffset.Now, TimeSpan.FromMilliseconds(200), "200", (j % 2 == 0) ? true : false);
                         }
                     });
@@ -74,6 +77,20 @@ namespace PerfTest
             }
 
             Console.WriteLine("Avge" + runs.Average());
+        }
+    }
+
+    internal class MyTelemetryInitializer : ITelemetryInitializer
+    {
+        private string currentValue;
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            currentValue = "mystring";
+            for (int i = 0; i < 100; i++)
+            {
+                telemetry.Context.Device.Id = currentValue;
+            }
         }
     }
 
