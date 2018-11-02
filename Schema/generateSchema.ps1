f00425ebdf9364416b91f448115b07f65fdc6e55
Cijo Thomas <cithomas@microsoft.com>
2018-11-01 21:38:42 -0700

    Request telemetry to use requestdata only on serialization

--- Schema/generateSchema.ps1
+++ Schema/generateSchema.ps1
@@ -117,6 +117,9 @@ dir "$currentDir\obj\gbc" | ForEach-Object {
 	RegExReplace $_.FullName "measurements = new ConcurrentDictionary<string, double>\(\);"
 }
 
+	# Remove "properties" field declaration as its is done lazy in a separate partial class
+    RegExReplace "$currentDir\obj\gbc\RequestData_types.cs" "public IDictionary<string, string> properties { get; set; }"
+	RegExReplace "$currentDir\obj\gbc\RequestData_types.cs" "properties = new ConcurrentDictionary<string, string>\(\);"
 
 #################################################################################################
 ## Use TimeSpan instead of String for duration to improve performance by avoiding conversions.
--- src/Microsoft.ApplicationInsights/Extensibility/Implementation/External/RequestData_typeslazy.cs
+++ src/Microsoft.ApplicationInsights/Extensibility/Implementation/External/RequestData_typeslazy.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Threading;
 
     /// <summary>
     /// Partial class to implement ISerializableWithWriter
@@ -9,13 +10,20 @@
     internal partial class RequestData
     {
         private IDictionary<string, double> measurementsInternal;
+        private IDictionary<string, string> propertiesInternal;
 
 #pragma warning disable SA1300 // Element must begin with upper-case letter
         public IDictionary<string, double> measurements
 #pragma warning restore SA1300 // Element must begin with upper-case letter
         {
-            get { return System.Threading.LazyInitializer.EnsureInitialized(ref this.measurementsInternal, () => new ConcurrentDictionary<string, double>()); }
+            get { return LazyInitializer.EnsureInitialized(ref this.measurementsInternal, () => new ConcurrentDictionary<string, double>()); }
             set { this.measurementsInternal = value; }
+        }
+
+        public IDictionary<string, string> properties
+        {
+            get { return LazyInitializer.EnsureInitialized(ref this.propertiesInternal, () => new ConcurrentDictionary<string, string>()); }
+            set { this.propertiesInternal = value; }
         }
     }
 }
