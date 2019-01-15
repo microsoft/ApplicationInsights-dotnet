41dc8af18c8dc181874eb7f99b526d42da46beb4
Cijo Thomas <cithomas@microsoft.com>
2019-01-15 07:48:21 -0800

    e2e test

---
+++ Test/ServerTelemetryChannel.Test/Shared.Tests/Helpers/LocalInProcHttpServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.ApplicationInsights.WindowsServer.Channel.Helpers
+{
+    public sealed class LocalInProcHttpServer : IDisposable
+    {
+        private readonly IWebHost host;
+        private readonly CancellationTokenSource cts;
+
+        public RequestDelegate ServerLogic;
+
+        public LocalInProcHttpServer(string url)
+        {
+            this.cts = new CancellationTokenSource();
+            this.host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls(url)
+                .Configure(
+                (app) =>
+                {
+                    app.Run(ServerLogic);
+                }
+                )
+                .Build();
+
+            Task.Run(() => this.host.RunAsync(this.cts.Token));
+        }
+
+        public void Dispose()
+        {
+            this.cts.Cancel(false);
+            try
+            {
+                this.host.Dispose();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+
+}
---
+++ Test/ServerTelemetryChannel.Test/Shared.Tests/ServerTelemetryChannelE2ETests.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer.Channel
+{
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Threading;
+
+    [TestClass]
+    public class ServerTelemetryChannelE2ETests
+    {        
+        [TestMethod]
+        public void InitializesTransmitterWithNetworkAvailabilityPolicy()
+        {
+
+            var channel = new ServerTelemetryChannel();
+            var config = new TelemetryConfiguration("dummy");
+
+            channel.Initialize(config);
+            Thread.Sleep(50);
+
+            Assert.AreEqual(0, channel.Transmitter.Sender.Capacity);
+        }
+    }
+}
