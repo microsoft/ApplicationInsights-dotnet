e03f58c860d3a17d79003adca8a95eedd047b7aa
Liudmila Molkova <lmolkova@microsoft.com>
2019-01-17 20:05:03 -0800

    Move common W3C classes from web to base

---
+++ src/Microsoft.ApplicationInsights/W3C/W3COperationCorrelationTelemetryInitializer.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.ApplicationInsights.W3C
+{
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.Extensibility;
+
+    /// <summary>
+    /// Telemetry Initializer that sets correlation ids for W3C.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class W3COperationCorrelationTelemetryInitializer : ITelemetryInitializer
+    {
+        /// <summary>
+        /// Initializes telemetry item.
+        /// </summary>
+        /// <param name="telemetry">Telemetry item.</param>
+        public void Initialize(ITelemetry telemetry)
+        {
+            Activity currentActivity = Activity.Current;
+            currentActivity.UpdateTelemetry(telemetry, false);
+        }
+    }
+}
