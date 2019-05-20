57a974c49ba74ff19c08e5f10653e8bb6ee23836
Timothy Mothra Lee <tilee@microsoft.com>
2019-05-20 14:22:36 -0700

    removing example class

--- Test/Microsoft.ApplicationInsights.Test/Shared/Extensibility/Implementation/Experimental/ExperimentalFeaturesTests.cs
+++ Test/Microsoft.ApplicationInsights.Test/Shared/Extensibility/Implementation/Experimental/ExperimentalFeaturesTests.cs
@@ -28,27 +28,5 @@ namespace Microsoft.ApplicationInsights.Extensibility.Implementation.Experimenta
             bool fakeValue = config.EvaluateExperimentalFeature("fake");
             Assert.IsFalse(fakeValue);
         }
-
-        [TestMethod]
-        public void VerifyExperimentalFeaturesClass()
-        {
-            // test 1: feature does not exist
-            var config1 = new TelemetryConfiguration
-            {
-                ExperimentalFeatures = new string[] { "abc" }
-            };
-
-            var value1 = ExperimentalFeatures.IsExampleFeatureEnabled(config1);
-            Assert.IsFalse(value1);
-
-            // test2: feature does exist
-            var config2 = new TelemetryConfiguration
-            {
-                ExperimentalFeatures = new string[] { "exampleFeature" }
-            };
-
-            var value2 = ExperimentalFeatures.IsExampleFeatureEnabled(config2);
-            Assert.IsTrue(value2);
-        }
     }
 }
