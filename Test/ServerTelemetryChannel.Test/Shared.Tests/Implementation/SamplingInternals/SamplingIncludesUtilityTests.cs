d8efc8b8a30274f3bd539dbd7617b905ec700dfb
Timothy Mothra Lee <tilee@microsoft.com>
2019-07-10 13:12:22 -0700

    working example

---
+++ Test/ServerTelemetryChannel.Test/Shared.Tests/Implementation/SamplingInternals/SamplingIncludesUtilityTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer.Channel.Implementation.SamplingInternals
+{
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel.Implementation.SamplingInternals;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SamplingIncludesUtilityTests
+    {
+        [TestMethod]
+        public void VerifySplitInput()
+        {
+            string input = "A;B;C;;";
+            string[] expected = { "A", "B", "C" };
+
+            var test = SamplingIncludesUtility.SplitInput(input);
+
+            Assert.AreEqual(3, test.Length);
+            CollectionAssert.AreEqual(expected, test);
+        }
+
+        [TestMethod]
+        public void VerifyIncludes()
+        {
+            string input = "DEPENDENCY;EVENT";
+
+            var test = SamplingIncludesUtility.CalculateFromIncludes(input);
+
+            Assert.IsTrue(test.HasFlag(SamplingTelemetryItemTypes.RemoteDependency));
+            Assert.IsTrue(test.HasFlag(SamplingTelemetryItemTypes.Event));
+            Assert.IsFalse(test.HasFlag(SamplingTelemetryItemTypes.Exception));
+            Assert.IsFalse(test.HasFlag(SamplingTelemetryItemTypes.PageView));
+            Assert.IsFalse(test.HasFlag(SamplingTelemetryItemTypes.Request));
+            Assert.IsFalse(test.HasFlag(SamplingTelemetryItemTypes.Message));
+        }
+
+        [TestMethod]
+        public void VerifyExcludes()
+        {
+            string input = "DEPENDENCY;EVENT";
+
+            var test = SamplingIncludesUtility.CalculateFromExcludes(input);
+
+            Assert.IsFalse(test.HasFlag(SamplingTelemetryItemTypes.RemoteDependency));
+            Assert.IsFalse(test.HasFlag(SamplingTelemetryItemTypes.Event));
+            Assert.IsTrue(test.HasFlag(SamplingTelemetryItemTypes.Exception));
+            Assert.IsTrue(test.HasFlag(SamplingTelemetryItemTypes.PageView));
+            Assert.IsTrue(test.HasFlag(SamplingTelemetryItemTypes.Request));
+            Assert.IsTrue(test.HasFlag(SamplingTelemetryItemTypes.Message));
+        }
+    }
+}
---
+++ src/ServerTelemetryChannel/Implementation/SamplingInternals/SamplingIncludesUtility.cs
@@ -0,0 +1,63 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel.Implementation.SamplingInternals
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    internal static class SamplingIncludesUtility
+    {
+        private const string DependencyTelemetryName = "DEPENDENCY";
+        private const string EventTelemetryName = "EVENT";
+        private const string ExceptionTelemetryName = "EXCEPTION";
+        private const string PageViewTelemetryName = "PAGEVIEW";
+        private const string RequestTelemetryName = "REQUEST";
+        private const string TraceTelemetryName = "TRACE";
+
+        private static readonly char[] ListSeparators = { ';' };
+        private static readonly IDictionary<string, SamplingTelemetryItemTypes> AllowedTypes;
+
+        static SamplingIncludesUtility()
+        {
+            AllowedTypes = new Dictionary<string, SamplingTelemetryItemTypes>(6, StringComparer.OrdinalIgnoreCase)
+            {
+                { DependencyTelemetryName, SamplingTelemetryItemTypes.RemoteDependency }, //DependencyTelemetry
+                { EventTelemetryName, SamplingTelemetryItemTypes.Event }, //EventTelemetry
+                { ExceptionTelemetryName, SamplingTelemetryItemTypes.Exception }, //ExceptionTelemetry
+                { PageViewTelemetryName, SamplingTelemetryItemTypes.PageView }, //PageViewTelemetry
+                { RequestTelemetryName, SamplingTelemetryItemTypes.Request }, //RequestTelemetry
+                { TraceTelemetryName, SamplingTelemetryItemTypes.Message }, //TraceTelemetry
+            };
+        }
+
+        public static SamplingTelemetryItemTypes CalculateFromIncludes(string includesString)
+        {
+            return Calculate(operation: IncludeOperator, flags: SamplingTelemetryItemTypes.None, input: includesString);
+        }
+
+        public static SamplingTelemetryItemTypes CalculateFromExcludes(string excludesString)
+        {
+            return Calculate(operation: ExcludeOperator, flags: ~SamplingTelemetryItemTypes.None, input: excludesString);
+        }
+
+        private static SamplingTelemetryItemTypes Calculate(Func<SamplingTelemetryItemTypes, SamplingTelemetryItemTypes, SamplingTelemetryItemTypes> operation, SamplingTelemetryItemTypes flags, string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                foreach (string item in SplitInput(input))
+                {
+                    if (AllowedTypes.TryGetValue(item, out SamplingTelemetryItemTypes value))
+                    {
+                        flags = operation(flags, value);
+                    }
+                }
+            }
+
+            return flags;
+        }
+
+        public static SamplingTelemetryItemTypes IncludeOperator(SamplingTelemetryItemTypes flags, SamplingTelemetryItemTypes value) => flags |= value;
+        public static SamplingTelemetryItemTypes ExcludeOperator(SamplingTelemetryItemTypes flags, SamplingTelemetryItemTypes value) => flags &= ~value;
+
+        internal static string[] SplitInput(string input) => input.Split(ListSeparators, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
