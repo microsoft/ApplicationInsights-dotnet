53259f78410f0610a84bcd1caa6be5d663c437d1
Dmitry Matveev <dmitmatv@microsoft.com>
2019-07-22 15:46:51 -0700

    Multiple hosts fix

--- src/Microsoft.ApplicationInsights.AspNetCore/Extensions/HttpRequestExtensions.cs
+++ src/Microsoft.ApplicationInsights.AspNetCore/Extensions/HttpRequestExtensions.cs
@@ -1,7 +1,6 @@
 ﻿namespace Microsoft.ApplicationInsights.AspNetCore.Extensions
 {
     using System;
-    using System.Text;
     using Microsoft.AspNetCore.Http;
 
     /// <summary>
@@ -10,6 +9,8 @@
     public static class HttpRequestExtensions
     {
         private const string UnknownHostName = "UNKNOWN-HOST";
+        private const string MultipleHostName = "MULTIPLE-HOST";
+        private const string Comma = ",";
 
         /// <summary>
         /// Gets http request Uri from request object.
@@ -31,7 +32,7 @@
             return new Uri(string.Concat(
                     request.Scheme,
                     "://",
-                    request.Host.HasValue ? request.Host.Value : UnknownHostName,
+                    request.Host.HasValue ? (request.Host.Value.IndexOf(Comma) > 0 ? MultipleHostName : request.Host.Value) : UnknownHostName,
                     request.Path.HasValue ? request.Path.Value : string.Empty,
                     request.QueryString.HasValue ? request.QueryString.Value : string.Empty));
         }
