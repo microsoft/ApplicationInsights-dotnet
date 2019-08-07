646f39086bddace304a84a2a5dfc4c98a15aad8c
Timothy Mothra Lee <tilee@microsoft.com>
2019-08-07 16:29:03 -0700

    enum extension

---
+++ Test/Microsoft.ApplicationInsights.Test/Shared/Common/EnumExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ApplicationInsights.Common.Extensions;
+
+namespace Microsoft.ApplicationInsights.Common
+{
+    [TestClass]
+    public class EnumExtensionsTests
+    {
+        [TestMethod]
+        public void TestEnumGetAttribute()
+        {
+            var test1 = TestEnum.One.GetAttribute<TestEnumAttribute>();
+            Assert.AreEqual("111", test1.Value);
+
+            var test2 = TestEnum.Two.GetAttribute<TestEnumAttribute>();
+            Assert.AreEqual("222", test2.Value);
+        }
+
+        private enum TestEnum
+        {
+            [TestEnum(Value = "111")]
+            One,
+
+            [TestEnum(Value = "222")]
+            Two,
+        }
+
+        private class TestEnumAttribute : Attribute
+        {
+            public string Value { get; set; }
+        }
+    }
+}
---
+++ src/Common/Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.ApplicationInsights.Common.Extensions
+{
+    using System;
+    using System.Reflection;
+
+    internal static class EnumExtensions
+    {
+        public static TAttribute GetAttribute<TAttribute>(this Enum value) where TAttribute : Attribute
+        {
+#if NETSTANDARD1_3
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            return type.GetRuntimeField(name).GetCustomAttribute<TAttribute>();
+#else
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            return type.GetField(name).GetCustomAttribute<TAttribute>();
+#endif
+        }
+    }
+}
