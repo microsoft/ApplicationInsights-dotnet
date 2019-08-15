3ed0cf270f598434399e08de630e724cb987062f
Timothy Mothra <tilee@microsoft.com>
2019-08-15 12:57:45 -0700

    Update src/Microsoft.ApplicationInsights/Extensibility/Implementation/Endpoints/ConnectionStringDuplicateKeyException.cs
    Co-Authored-By: Reiley Yang <reyang@microsoft.com>

--- src/Microsoft.ApplicationInsights/Extensibility/Implementation/Endpoints/ConnectionStringDuplicateKeyException.cs
+++ src/Microsoft.ApplicationInsights/Extensibility/Implementation/Endpoints/ConnectionStringDuplicateKeyException.cs
@@ -32,7 +32,7 @@
         /// Initializes a new instance of the <see cref="ConnectionStringDuplicateKeyException"/> class.
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
-        /// <param name = "innerException" > The exception that is the cause of the current exception, or a null reference(Nothing in Visual Basic) if no inner exception is specified.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
         public ConnectionStringDuplicateKeyException(string message, Exception innerException) : base(message, innerException)
         {
         }
