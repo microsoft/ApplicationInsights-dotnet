40b25d3a1bde9ca5f4dd3bc11345ce57c08b9315
Timothy Mothra Lee <tilee@microsoft.com>
2019-10-24 14:30:15 -0700

    cleanup

--- BASE/Test/Microsoft.ApplicationInsights.Test/Shared/Extensibility/Implementation/InstrumentationKeyValidationTests.cs
+++ BASE/Test/Microsoft.ApplicationInsights.Test/Shared/Extensibility/Implementation/InstrumentationKeyValidationTests.cs
@@ -9,33 +9,31 @@
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void VerifyEmptyIkeyIsInvalid() => RunTest(string.Empty);
+        public void VerifyEmptyIkeyIsInvalid() => InstrumentationKeyValidation.Validate(string.Empty);
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void VerifyNullIkeyIsInvalid() => RunTest(null);
+        public void VerifyNullIkeyIsInvalid() => InstrumentationKeyValidation.Validate(null);
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void VerifyWhitespaceIkeyIsInvalid() => RunTest("   ");
+        public void VerifyWhitespaceIkeyIsInvalid() => InstrumentationKeyValidation.Validate("   ");
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void VerifyExtraQuotesIkeyIsInvalid() => RunTest("\"00000000-0000-0000-0000-000000000000\"");
+        public void VerifyExtraQuotesIkeyIsInvalid() => InstrumentationKeyValidation.Validate("\"00000000-0000-0000-0000-000000000000\"");
         
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "Instrumentation Key contains non-printable characters.")]
-        public void VerifyNonPrintableCharacterIkeyIsInvalid() => RunTest("00000000-0000-0000-0000-000000000000\r\n");
+        public void VerifyNonPrintableCharacterIkeyIsInvalid() => InstrumentationKeyValidation.Validate("00000000-0000-0000-0000-000000000000\r\n");
         
         [TestMethod]
-        public void VerifyExample1() => RunTest("AAA-00000000-0000-0000-0000-000000000000");
+        public void VerifyExample1() => InstrumentationKeyValidation.Validate("AAA-00000000-0000-0000-0000-000000000000");
         
         [TestMethod]
-        public void VerifyExample2() => RunTest("00000000-0000-0000-0000-000000000000");
+        public void VerifyExample2() => InstrumentationKeyValidation.Validate("00000000-0000-0000-0000-000000000000");
         
         [TestMethod]
-        public void VerifyExample3() => RunTest("a:00000000-0000-0000-0000-000000000000");
-
-        private void RunTest(string input) => InstrumentationKeyValidation.Validate(input);
+        public void VerifyExample3() => InstrumentationKeyValidation.Validate("a:00000000-0000-0000-0000-000000000000");
     }
 }
