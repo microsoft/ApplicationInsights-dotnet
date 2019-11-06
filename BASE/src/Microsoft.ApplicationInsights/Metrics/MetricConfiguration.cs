c57ff0788995ec5d3f7971ccb3247ffb725b32a8
Timothy Mothra Lee <tilee@microsoft.com>
2019-11-05 16:46:47 -0800

    missing using

--- BASE/src/Microsoft.ApplicationInsights/Metrics/MetricConfiguration.cs
+++ BASE/src/Microsoft.ApplicationInsights/Metrics/MetricConfiguration.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using static System.FormattableString;
 
     /// <summary>Encapsulates the configuration for a metric and its respective data time series.</summary>
