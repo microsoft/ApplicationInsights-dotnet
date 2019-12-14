3607795117447045716e0c10914ae8061cf25c75
Timothy Mothra Lee <tilee@microsoft.com>
2019-12-13 18:02:12 -0800

    undo dispose

--- BASE/src/ServerTelemetryChannel/Implementation/TransmissionExtensions.cs
+++ BASE/src/ServerTelemetryChannel/Implementation/TransmissionExtensions.cs
@@ -15,40 +15,39 @@
         /// Loads a new transmission from the specified <paramref name="stream"/>.
         /// </summary>
         /// <returns>Return transmission loaded from file; throws FormatException is file is corrupted.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Disposing the StreamReader will also dispose the stream.")]
         public static Transmission Load(Stream stream)
         {
-            using (var reader = new StreamReader(stream))
-            {
-                Uri address = ReadAddress(reader);
-                string contentType = ReadHeader(reader, ContentTypeHeader);
-                string contentEncoding = ReadHeader(reader, ContentEncodingHeader);
-                byte[] content = ReadContent(reader);
-                return new Transmission(address, content, contentType, contentEncoding);
-            }
+            var reader = new StreamReader(stream);
+            Uri address = ReadAddress(reader);
+            string contentType = ReadHeader(reader, ContentTypeHeader);
+            string contentEncoding = ReadHeader(reader, ContentEncodingHeader);
+            byte[] content = ReadContent(reader);
+            return new Transmission(address, content, contentType, contentEncoding);
         }
+
 
         /// <summary>
         /// Saves the transmission to the specified <paramref name="stream"/>.
         /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Disposing the StreamWriter will also dispose the stream.")]
         public static void Save(this Transmission transmission, Stream stream)
         {
-            using (var writer = new StreamWriter(stream))
-            {
-                writer.WriteLine(transmission.EndpointAddress.ToString());
+            var writer = new StreamWriter(stream);
+            writer.WriteLine(transmission.EndpointAddress.ToString());
 
-                writer.Write(ContentTypeHeader);
-                writer.Write(":");
-                writer.WriteLine(transmission.ContentType);
+            writer.Write(ContentTypeHeader);
+            writer.Write(":");
+            writer.WriteLine(transmission.ContentType);
 
-                writer.Write(ContentEncodingHeader);
-                writer.Write(":");
-                writer.WriteLine(transmission.ContentEncoding);
+            writer.Write(ContentEncodingHeader);
+            writer.Write(":");
+            writer.WriteLine(transmission.ContentEncoding);
 
-                writer.WriteLine(string.Empty);
+            writer.WriteLine(string.Empty);
 
-                writer.Write(Convert.ToBase64String(transmission.Content));
-                writer.Flush();
-            }
+            writer.Write(Convert.ToBase64String(transmission.Content));
+            writer.Flush();
         }
 
         private static string ReadHeader(TextReader reader, string headerName)
