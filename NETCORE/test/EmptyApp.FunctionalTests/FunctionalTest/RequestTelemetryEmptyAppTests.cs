7146726f1d187786257bd9d6d045b15e1ed10579
Timothy Mothra Lee <tilee@microsoft.com>
2019-12-30 15:28:28 -0800

    missing using

--- NETCORE/test/EmptyApp.FunctionalTests/FunctionalTest/RequestTelemetryEmptyAppTests.cs
+++ NETCORE/test/EmptyApp.FunctionalTests/FunctionalTest/RequestTelemetryEmptyAppTests.cs
@@ -2,6 +2,7 @@
 {
     using System.Linq;
     using System.Net.Http;
+    using System.Reflection;
     using FunctionalTestUtils;
     using Microsoft.ApplicationInsights.DataContracts;
     using Xunit;
