47f245786301bba8ab3623652da5366ea7318a4c
Timothy Mothra Lee <tilee@microsoft.com>
2019-12-30 16:13:47 -0800

    change expected ResponseCode from 200 to 500

--- NETCORE/test/WebApi.FunctionalTests/FunctionalTest/ExceptionTelemetryWebApiTests.cs
+++ NETCORE/test/WebApi.FunctionalTests/FunctionalTest/ExceptionTelemetryWebApiTests.cs
@@ -26,9 +26,7 @@
 
                 var expectedRequestTelemetry = new RequestTelemetry();
                 expectedRequestTelemetry.Name = "GET Exception/Get";
-                //TODO: default template of Web API application doesn't have error handling middleware 
-                //that will set appropriate status code
-                expectedRequestTelemetry.ResponseCode = "200";
+                expectedRequestTelemetry.ResponseCode = "500";
                 expectedRequestTelemetry.Success = false;
                 expectedRequestTelemetry.Url = new System.Uri(server.BaseHost + RequestPath);
 
