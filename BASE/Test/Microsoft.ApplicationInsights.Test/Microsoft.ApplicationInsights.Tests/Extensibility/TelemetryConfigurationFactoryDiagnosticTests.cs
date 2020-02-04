070403188ecd030242aa53c98754aaaefe43fa98
Timothy Mothra Lee <tilee@microsoft.com>
2020-02-03 16:53:20 -0800

    new test

--- BASE/Test/Microsoft.ApplicationInsights.Test/Microsoft.ApplicationInsights.Tests/Extensibility/TelemetryConfigurationFactoryDiagnosticTests.cs
+++ BASE/Test/Microsoft.ApplicationInsights.Test/Microsoft.ApplicationInsights.Tests/Extensibility/TelemetryConfigurationFactoryDiagnosticTests.cs
@@ -2,8 +2,10 @@
 {
 #if !NETCOREAPP1_1
     using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
-
+    using System.Xml.Linq;
     using Microsoft.ApplicationInsights.Extensibility.Implementation.Platform;
     using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing.SelfDiagnostics;
     using Microsoft.ApplicationInsights.TestFramework;
@@ -82,6 +84,46 @@
             }
         }
 
+        [TestMethod]
+        public void WhatHappensWithMultipleModules()
+        {
+            string testLogFilePath1 = "C:\\Temp\\111";
+            string testLogFilePath2 = "C:\\Temp\\222";
+
+            // SETUP
+            var platform = new StubEnvironmentVariablePlatform();
+            platform.SetEnvironmentVariable(TelemetryConfigurationFactory.DiagnosticsEnvironmentVariable, "true");
+            platform.SetEnvironmentVariable(TelemetryConfigurationFactory.DiagnosticsLogDirectoryEnvironmentVariable, testLogFilePath2);
+            PlatformSingleton.Current = platform;
+
+            string configFileContents = Configuration(
+                @"<TelemetryModules>
+                    <Add Type = """ + typeof(FileDiagnosticsTelemetryModule).AssemblyQualifiedName + @"""  >
+                        <LogFilePath>" + testLogFilePath1 + @"</LogFilePath>
+                    </Add>
+                    <Add Type = """ + typeof(FileDiagnosticsTelemetryModule).AssemblyQualifiedName + @"""  >
+                        <LogFilePath>xxx</LogFilePath>
+                    </Add>
+                  </TelemetryModules>");
+
+            using (var modules = new TestableTelemetryModules())
+            {
+                new TestableTelemetryConfigurationFactory().Initialize(new TelemetryConfiguration(), modules, configFileContents);
+
+                var module = modules.Modules.OfType<FileDiagnosticsTelemetryModule>().Single();
+                Assert.AreEqual(testLogFilePath2, module.LogFilePath, "We want the config from the environment variable to take precedence to enable DevOps to have control over troubleshooting scenarios");
+            }
+        }
+
+        private static string Configuration(string innerXml)
+        {
+            return
+              @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+                <ApplicationInsights xmlns=""http://schemas.microsoft.com/ApplicationInsights/2013/Settings"">
+" + innerXml + @"
+                </ApplicationInsights>";
+        }
+
         private void RunBasicTest(string environmentVariableValue, bool shouldInitializeModule)
         {
             // SETUP
@@ -108,6 +150,35 @@
                 {
                     (module as IDisposable)?.Dispose();
                 }
+            }
+        }
+
+        private class TestableTelemetryConfigurationFactory : TelemetryConfigurationFactory
+        {
+            public static object CreateInstance(Type interfaceType, string typeName)
+            {
+                return TelemetryConfigurationFactory.CreateInstance(interfaceType, typeName);
+            }
+
+            public static new void LoadFromXml(TelemetryConfiguration configuration, TelemetryModules modules, XDocument xml)
+            {
+                TelemetryConfigurationFactory.LoadFromXml(configuration, modules, xml);
+            }
+
+            public static object LoadInstance(XElement definition, Type expectedType, object instance, TelemetryModules modules)
+            {
+                return TelemetryConfigurationFactory.LoadInstance(definition, expectedType, instance, null, modules);
+            }
+
+            [SuppressMessage("Microsoft.Design", "CA1061:DoNotHideBaseClassMethods", Justification = "This method allows calling protected base method in this test class.")]
+            public static new void LoadInstances<T>(XElement definition, ICollection<T> instances, TelemetryModules modules)
+            {
+                TelemetryConfigurationFactory.LoadInstances(definition, instances, modules);
+            }
+
+            public static new void LoadProperties(XElement definition, object instance, TelemetryModules modules)
+            {
+                TelemetryConfigurationFactory.LoadProperties(definition, instance, modules);
             }
         }
     }
