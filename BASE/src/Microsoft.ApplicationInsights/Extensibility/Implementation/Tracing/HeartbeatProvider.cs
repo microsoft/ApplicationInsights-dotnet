5932e01e3c99e65d46496017e9d04ba0b2cfe331
Igor_Sychev <Igor_Sychev@epam.com>
2020-02-19 10:41:34 +0300

    timeinterval don't used. instead if private field used values from Provider
       public TimeSpan HeartbeatInterval
            {
                get => this.HeartbeatProvider.HeartbeatInterval;
                set => this.HeartbeatProvider.HeartbeatInterval = value;
            }

--- BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/HeartbeatProvider.cs
+++ BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/HeartbeatProvider.cs
@@ -4,7 +4,6 @@
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Globalization;
-    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.ApplicationInsights.Channel;
