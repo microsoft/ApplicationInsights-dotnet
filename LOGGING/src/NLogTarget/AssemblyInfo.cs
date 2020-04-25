e9ab4ca1360f3c348973daa1c2739e10accba42d
Eddy Nakamura <ednakamu@microsoft.com>
2020-04-25 06:49:25 -0300

    Merging NLogTarget.Tests

--- LOGGING/src/NLogTarget/AssemblyInfo.cs
+++ LOGGING/src/NLogTarget/AssemblyInfo.cs
@@ -30,8 +30,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("44abc834-bd19-449f-ad64-f8c83ca078ed")]
 
-[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.NLogTarget.Net45.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
-[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.NLogTarget.NetCoreApp10.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
+[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.NLogTarget.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
 
 internal static class AssemblyInfo
 {
