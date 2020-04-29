69cc565d30f2d8f570d6d938f7f54d8ab9165301
Eddy Nakamura <ednakamu@microsoft.com>
2020-04-29 15:31:32 -0300

    Re-adding missing dll; adding new assert to prevent dll exclusion

--- WEB/Src/WindowsServer/WindowsServer.Tests/AzureRoleEnvironmentTelemetryInitializerTest.cs
+++ WEB/Src/WindowsServer/WindowsServer.Tests/AzureRoleEnvironmentTelemetryInitializerTest.cs
@@ -1,11 +1,11 @@
 ﻿#if NET45
 namespace Microsoft.ApplicationInsights.WindowsServer
-{    
+{
     using System;
     using System.Diagnostics.Tracing;
     using System.Globalization;
     using System.IO;
-    using System.Linq;    
+    using System.Linq;
     using Microsoft.ApplicationInsights.DataContracts;
     using Microsoft.ApplicationInsights.Extensibility.Implementation;
     using Microsoft.ApplicationInsights.WindowsServer.Azure;
@@ -17,7 +17,7 @@ namespace Microsoft.ApplicationInsights.WindowsServer
 
     [TestClass]
     public class AzureRoleEnvironmentTelemetryInitializerTest
-    {        
+    {
         [TestMethod]
         public void AzureRoleEnvironmentTelemetryInitializerDoesNotOverrideRoleName()
         {
@@ -33,7 +33,7 @@ namespace Microsoft.ApplicationInsights.WindowsServer
         [TestMethod]
         public void AzureRoleEnvironmentTelemetryInitializerDoesNotOverrideRoleInstance()
         {
-            var telemetryItem = new EventTelemetry();            
+            var telemetryItem = new EventTelemetry();
             AzureRoleEnvironmentTelemetryInitializer initializer = new AzureRoleEnvironmentTelemetryInitializer();
             telemetryItem.Context.Cloud.RoleInstance = "Test";
             initializer.Initialize(telemetryItem);
@@ -55,7 +55,7 @@ namespace Microsoft.ApplicationInsights.WindowsServer
 
         [TestMethod]
         public void AzureRoleEnvironmentTelemetryInitializerSetsTelemetryContextPropertiesToNullWhenNotRunningInsideAzureCloudService()
-        {            
+        {
             // This test asssumes that it is not running inside a cloud service.
             // Its Ok even if Azure ServiceRunTime dlls are in the GAC, as IsAvailable() will return false, and hence 
             // no context will be further attempted to be read.
@@ -63,7 +63,7 @@ namespace Microsoft.ApplicationInsights.WindowsServer
             AzureRoleEnvironmentContextReader.Instance = null;
             AzureRoleEnvironmentContextReader.AssemblyLoaderType = null;
             AzureRoleEnvironmentTelemetryInitializer initializer = new AzureRoleEnvironmentTelemetryInitializer();
-            initializer.Initialize(telemetryItem);            
+            initializer.Initialize(telemetryItem);
 
             Assert.Null(telemetryItem.Context.Cloud.RoleName);
             Assert.Null(telemetryItem.Context.Cloud.RoleInstance);
@@ -94,13 +94,15 @@ namespace Microsoft.ApplicationInsights.WindowsServer
             }
         }
 
-        [TestMethod]        
+        [TestMethod]
         [Description("Validates that requested DLL was loaded into separate AppDomain and not to the current domain. This test will fail if not run with admin privileges.")]
         public void AzureRoleEnvironmentTelemetryInitializerLoadDllToSeparateAppDomain()
         {
             // A random dll which is not already loaded to the current AppDomain but dropped into bin folder.
             string dllPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "Microsoft.ApplicationInsights.Log4NetAppender.dll");
-                        
+
+            Assert.True(File.Exists(dllPath));
+
             try
             {
                 // Publish the dll to GAC to give a chance for  AzureRoleEnvironmentTelemetryInitializer to load it to a new AppDomain
@@ -109,7 +111,7 @@ namespace Microsoft.ApplicationInsights.WindowsServer
                 // Validate that the dll is not loaded to test AppDomaion to begin with.
                 var retrievedAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(item => string.Equals(item.GetName().Name, "Microsoft.ApplicationInsights.Log4NetAppender", StringComparison.OrdinalIgnoreCase));
                 Assert.Null(retrievedAssembly);
-                
+
                 // TestAssemblyLoader will load a random assembly (Microsoft.ApplicationInsights.Log4NetAppender.dll) and populate TestRoleName, TestRoleInstanceId into the fields.
                 AzureRoleEnvironmentContextReader.AssemblyLoaderType = typeof(TestAzureServiceRuntimeAssemblyLoader);
                 AzureRoleEnvironmentContextReader.Instance = null;
@@ -125,7 +127,7 @@ namespace Microsoft.ApplicationInsights.WindowsServer
                 var telemetryItem = new EventTelemetry();
                 initializer.Initialize(telemetryItem);
                 Assert.Equal("TestRoleName", telemetryItem.Context.Cloud.RoleName);
-                Assert.Equal("TestRoleInstanceId", telemetryItem.Context.Cloud.RoleInstance);              
+                Assert.Equal("TestRoleInstanceId", telemetryItem.Context.Cloud.RoleInstance);
             }
             finally
             {
