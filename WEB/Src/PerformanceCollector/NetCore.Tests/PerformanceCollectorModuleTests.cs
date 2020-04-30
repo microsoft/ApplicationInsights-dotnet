68c11e68595dcf3bbd8e6b75c6b1c6e028fca4c4
Eddy Nakamura <ednakamu@microsoft.com>
2020-04-30 16:46:12 -0300

    merging perf.shared.tests and NetCore.Tests

--- WEB/Src/PerformanceCollector/NetCore.Tests/PerformanceCollectorModuleTests.cs
+++ WEB/Src/PerformanceCollector/NetCore.Tests/PerformanceCollectorModuleTests.cs
@@ -1,25 +1,31 @@
-﻿#if NET45
-namespace Microsoft.ApplicationInsights.Tests
+﻿namespace Microsoft.ApplicationInsights.Tests
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
+    using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.Implementation;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#if NET45
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using System.Threading;
     using Microsoft.ApplicationInsights.DataContracts;
-    using Microsoft.ApplicationInsights.Extensibility;
-    using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
-    using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.Implementation;
-    using Microsoft.ApplicationInsights.TestFramework;
     using Microsoft.ApplicationInsights.Web.TestFramework;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
 
     /// <summary>
-    /// PerformanceCollectorModules tests.
+    /// PerformanceCollectorModuleTests tests.
+    /// The goal is to test that the default list contains only those counters which are supported.
+    /// Adding any unsupported counter by default will add noisy traces to user ikey.
     /// </summary>
     [TestClass]
-    public class PerformanceCollectorModulesTests
+    public class PerformanceCollectorModuleTests
     {
+#if NET45
         [TestMethod]
         [SuppressMessage(category: "Microsoft.Globalization", checkId: "CA1305:SpecifyIFormatProvider", Justification = "Don't care about invariant in unit tests.")]
         public void TimerTest()
@@ -31,25 +37,25 @@ namespace Microsoft.ApplicationInsights.Tests
             Exception assertionsFailure = null;
 
             telemetryChannel.OnSend = telemetry =>
+            {
+                // validate that a proper telemetry item is being sent
+                // module will swallow any exception that we throw here, so catch and rethrow later
+                try
                 {
-                    // validate that a proper telemetry item is being sent
-                    // module will swallow any exception that we throw here, so catch and rethrow later
-                    try
-                    {
-                        Assert.AreEqual(configuration.InstrumentationKey, telemetry.Context.InstrumentationKey);
+                    Assert.AreEqual(configuration.InstrumentationKey, telemetry.Context.InstrumentationKey);
 
-                        Assert.IsInstanceOfType(telemetry, typeof(MetricTelemetry));
+                    Assert.IsInstanceOfType(telemetry, typeof(MetricTelemetry));
 
-                        var perfTelemetry = telemetry as MetricTelemetry;
-                        
-                        Assert.AreEqual((double)perfTelemetry.Name.GetHashCode(), perfTelemetry.Sum);
-                    }
-                    catch (AssertFailedException e)
-                    {
-                        // race condition, but we don't care who wins
-                        assertionsFailure = e;
-                    }
-                };
+                    var perfTelemetry = telemetry as MetricTelemetry;
+
+                    Assert.AreEqual((double)perfTelemetry.Name.GetHashCode(), perfTelemetry.Sum);
+                }
+                catch (AssertFailedException e)
+                {
+                    // race condition, but we don't care who wins
+                    assertionsFailure = e;
+                }
+            };
 
             using (var module = CreatePerformanceCollectionModule(collector))
             {
@@ -105,7 +111,7 @@ namespace Microsoft.ApplicationInsights.Tests
             var collector = CreatePerformanceCollector();
 
             var configuration = CreateTelemetryConfiguration();
-            
+
             using (var module = CreatePerformanceCollectionModule(collector))
             {
                 // start the module
@@ -327,7 +333,7 @@ namespace Microsoft.ApplicationInsights.Tests
                 // make the module think that initial binding has already happened and it's not time to rebind yet
                 var privateObject = new PrivateObject(module);
                 privateObject.SetField("lastRefreshTimestamp", DateTime.Now + TimeSpan.FromMinutes(1));
-                
+
                 // wait 1s to let the module finish initializing
                 Thread.Sleep(TimeSpan.FromSeconds(1));
 
@@ -388,6 +394,127 @@ namespace Microsoft.ApplicationInsights.Tests
 
             return module;
         }
+#endif
+
+        [TestMethod]
+        public void PerformanceCollectorModuleDefaultContainsExpectedCountersNonWindows()
+        {
+#if NETCOREAPP2_0
+            PerformanceCounterUtility.isAzureWebApp = null;
+            var original = PerformanceCounterUtility.IsWindows;
+            PerformanceCounterUtility.IsWindows = false;
+            var module = new PerformanceCollectorModule();
+            
+            try
+            {                                          
+                module.Initialize(new TelemetryConfiguration());
+
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\% Processor Time"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\% Processor Time Normalized"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\Private Bytes"));
+                Assert.AreEqual(3, module.DefaultCounters.Count);
+            }
+            finally
+            {
+                PerformanceCounterUtility.IsWindows = original;
+                module.Dispose();
+            }
+#endif
+        }
+
+        [TestMethod]
+        public void PerformanceCollectorModuleDefaultContainsExpectedCountersWebApps()
+        {
+            PerformanceCounterUtility.isAzureWebApp = null;
+            Environment.SetEnvironmentVariable("WEBSITE_SITE_NAME", "something");
+            var module = new PerformanceCollectorModule();
+            try
+            {
+                module.Initialize(new TelemetryConfiguration());
+
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\% Processor Time"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\% Processor Time Normalized"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\Private Bytes"));
+
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Memory\Available Bytes"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\IO Data Bytes/sec"));
+
+#if NET45
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests/Sec"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\.NET CLR Exceptions(??APP_CLR_PROC??)\# of Exceps Thrown / sec"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Request Execution Time"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests In Application Queue"));
+                Assert.AreEqual(9, module.DefaultCounters.Count);
+#else                
+                Assert.AreEqual(5, module.DefaultCounters.Count);
+#endif
+            }
+            finally
+            {
+                PerformanceCounterUtility.isAzureWebApp = null;
+                module.Dispose();
+                Environment.SetEnvironmentVariable("WEBSITE_SITE_NAME", string.Empty);
+                Task.Delay(1000).Wait();
+            }
+        }
+
+        [TestMethod]
+        public void PerformanceCollectorModuleDefaultContainsExpectedCountersWindows()
+        {
+            PerformanceCounterUtility.isAzureWebApp = null;
+            var module = new PerformanceCollectorModule();
+#if NETCOREAPP2_0
+            var original = PerformanceCounterUtility.IsWindows;
+            PerformanceCounterUtility.IsWindows = true;
+#endif
+            try
+            {
+                module.Initialize(new TelemetryConfiguration());
+#if !NETCOREAPP1_0
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\% Processor Time"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\% Processor Time Normalized"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\Private Bytes"));
+
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Memory\Available Bytes"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Process(??APP_WIN32_PROC??)\IO Data Bytes/sec"));
+
+#if NET45
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests/Sec"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\.NET CLR Exceptions(??APP_CLR_PROC??)\# of Exceps Thrown / sec"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Request Execution Time"));
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\ASP.NET Applications(??APP_W3SVC_PROC??)\Requests In Application Queue"));
+#endif
+
+                Assert.IsTrue(ContainsPerfCounter(module.DefaultCounters, @"\Processor(_Total)\% Processor Time"));
+#if NET45
+                Assert.AreEqual(10, module.DefaultCounters.Count);
+#else
+                Assert.AreEqual(6, module.DefaultCounters.Count);
+#endif
+
+#endif
+
+            }
+            finally
+            {
+                module.Dispose();
+#if NETCOREAPP2_0
+            PerformanceCounterUtility.IsWindows = original;
+#endif
+            }
+        }
+
+        private bool ContainsPerfCounter(IList<PerformanceCounterCollectionRequest> counters, string name)
+        {
+            foreach (var counter in counters)
+            {
+                if (counter.PerformanceCounter.Equals(name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
-#endif
