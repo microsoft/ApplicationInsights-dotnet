05a65f16754c34db52d60c1e16ff4622982f9939
Timothy Mothra Lee <tilee@microsoft.com>
2020-05-04 14:03:16 -0700

    refactor aspnetcore tests (part1) move helper classes into separate file.

---
+++ NETCORE/test/Microsoft.ApplicationInsights.AspNetCore.Tests/Extensions/ApplicationInsightsExtensionsTests/IServiceProviderExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Extensions.DependencyInjection.Test
+{
+    using System;
+
+    using Microsoft.ApplicationInsights.Extensibility;
+
+    using Microsoft.Extensions.Options;
+
+    internal static class IServiceProviderExtensions
+    {
+        public static TelemetryConfiguration GetTelemetryConfiguration(this IServiceProvider serviceProvider)
+        {
+            return serviceProvider.GetRequiredService<IOptions<TelemetryConfiguration>>().Value;
+        }
+    }
+}
