f92bf55b4c337368f1089f92312b04f2343379e0
Timothy Mothra <tilee@microsoft.com>
2020-06-04 17:45:31 -0700

    refactor Azure Heartbeat Modules (#1897)
    * refactor Heartbeat
    
    * cleanup IsInitialized
    
    * fix compile error
    
    * fixing one more
    
    * fixing more tests

---
+++ WEB/Src/WindowsServer/WindowsServer/Implementation/HeartbeatPropertyManagerProvider.cs
@@ -0,0 +1,36 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer.Implementation
+{
+    using System;
+
+    using Microsoft.ApplicationInsights.Extensibility.Implementation;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing;
+
+    internal static class HeartbeatPropertyManagerProvider
+    {
+        public static IHeartbeatPropertyManager GetHeartbeatPropertyManager()
+        {
+            var telemetryModules = TelemetryModules.Instance;
+            if (telemetryModules != null)
+            {
+                try
+                {
+                    foreach (var module in telemetryModules.Modules)
+                    {
+                        if (module is IHeartbeatPropertyManager hman)
+                        {
+                            return hman;
+                        }
+                    }
+                }
+                catch (Exception hearbeatManagerAccessException)
+                {
+                    WindowsServerEventSource.Log.AppServiceHeartbeatManagerAccessFailure(hearbeatManagerAccessException.ToInvariantString());
+                }
+            }
+
+            // Module was not found. Log and return null.
+            WindowsServerEventSource.Log.AppServiceHeartbeatManagerNotAvailable();
+            return null;
+        }
+    }
+}
