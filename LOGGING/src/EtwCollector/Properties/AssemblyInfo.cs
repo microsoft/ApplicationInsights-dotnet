eba765f7fa2bfc2afbd1e33de4f34094d8f33146
Eddy Nakamura <eddynaka@gmail.com>
2020-06-16 17:33:26 -0300

    removing useless properties in *.csproj (#1922)
    * removing useless properties in *.csproj
    
    removing more tags
    
    removing more tags
    
    updating nupkg
    
    updating assemblyinfo
    
    Revert "updating assemblyinfo"
    
    This reverts commit 37e7b4b45e79d96ba723528e7d8b7fa3be8a6fa0.
    
    * removing extra tags
    
    Co-authored-by: Timothy Mothra <tilee@microsoft.com>

--- LOGGING/src/EtwCollector/Properties/AssemblyInfo.cs
+++ LOGGING/src/EtwCollector/Properties/AssemblyInfo.cs
@@ -9,14 +9,6 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.ApplicationInsights.EtwCollector")]
-[assembly: AssemblyDescription("")]
-
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
