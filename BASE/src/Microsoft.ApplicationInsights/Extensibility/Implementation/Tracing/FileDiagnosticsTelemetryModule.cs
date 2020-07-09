e94a1dec57143e2a8ba46ec37b94f9f2c92065da
Timothy Mothra Lee <tilee@microsoft.com>
2020-07-08 17:55:58 -0700

    fix

--- BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/FileDiagnosticsTelemetryModule.cs
+++ BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/FileDiagnosticsTelemetryModule.cs
@@ -4,7 +4,6 @@
     using System.Diagnostics;
     using System.Diagnostics.Tracing;
     using System.IO;
-    using System.Runtime.CompilerServices;
     using System.Security;
     using System.Threading;
 
