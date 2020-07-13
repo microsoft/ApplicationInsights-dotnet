7b46e0205bb7dc095184e214d643db4bb452cb5e
Timothy Mothra Lee <tilee@microsoft.com>
2020-07-13 11:36:46 -0700

    refactor FileDiagnostics. moving code to new FileDiagnosticsSender

--- BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/FileDiagnosticsTelemetryModule.cs
+++ BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/FileDiagnosticsTelemetryModule.cs
@@ -8,6 +8,7 @@
     using System.Threading;
 
     using Microsoft.ApplicationInsights.Common.Extensions;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation.Platform;
     using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing.FileDiagnosticsModule;
 
     using static System.FormattableString;
@@ -19,8 +20,7 @@
     {
         private readonly TraceSourceForEventSource traceSource = new TraceSourceForEventSource(EventLevel.Error);
         private readonly DefaultTraceListener listener = new DefaultTraceListener();
-
-        private string windowsIdentityName;
+        
         private string logFileName;
         private string logFilePath;
 
@@ -96,14 +96,6 @@
         }
 
         /// <summary>
-        /// Gets or sets the self-diagnostics configuration string used to setup this module.
-        /// </summary>
-        /// <remarks>
-        /// This module can be setup by the Self-Diagnostics Environment Variable and we want to include that string in the file.
-        /// </remarks>
-        internal string SelfDiagnosticsConfig { get; set; }
-
-        /// <summary>
         /// No op.
         /// </summary>
         /// <param name="configuration">Telemetry configuration object.</param>
@@ -132,40 +124,6 @@
             }
         }
 
-        /// <summary>
-        /// Create a random named file and write to it to confirm that we have access to this directory. 
-        /// Deletes flie when FileStream is closed.
-        /// </summary>
-        /// <exception cref="UnauthorizedAccessException">Throws <see cref="UnauthorizedAccessException" /> if the process lacks the required permissions to access the <paramref name="directory"/>.</exception>
-        private static void CheckAccessPermissions(DirectoryInfo directory)
-        {
-            string testFileName = Path.GetRandomFileName();
-            string testFilePath = Path.Combine(directory.FullName, testFileName);
-
-            if (!Directory.Exists(directory.FullName))
-            {
-                Directory.CreateDirectory(directory.FullName);
-            }
-
-            // Create a test file 
-            using (var testFile = new FileStream(testFilePath, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.None, 4096, FileOptions.DeleteOnClose))
-            {
-                testFile.Write(new[] { default(byte) }, 0, 1);
-            }
-        }
-
-        private void WriteFileHeader(string logFilePath)
-        {
-            string[] lines = 
-            { 
-                this.SelfDiagnosticsConfig,
-                ".NET SDK version: " + SdkVersionUtils.GetSdkVersion(string.Empty), 
-                string.Empty,
-            };
-
-            System.IO.File.WriteAllLines(logFilePath, lines);
-        }
-
         private bool SetAndValidateLogsFolder(string filePath, string fileName)
         {
             bool result = false;
@@ -175,12 +133,10 @@
                 {
                     var logsDirectory = new DirectoryInfo(filePath);
 
-                    CheckAccessPermissions(logsDirectory);
+                    PlatformSingleton.Current.TestDirectoryPermissions(logsDirectory);
 
                     string fullLogFileName = Path.Combine(filePath, fileName);
                     CoreEventSource.Log.LogsFileName(fullLogFileName);
-
-                    this.WriteFileHeader(fullLogFileName);
 
                     this.listener.LogFileName = fullLogFileName;
 
@@ -198,30 +154,7 @@
 
                 CoreEventSource.Log.LogStorageAccessDeniedError(
                     error: Invariant($"Path: {this.logFilePath} File: {this.logFileName}; Error: {ex.Message}{Environment.NewLine}"),
-                    user: LazyInitializer.EnsureInitialized(ref this.windowsIdentityName, this.GetCurrentIdentityName));
-            }
-
-            return result;
-        }
-
-        private string GetCurrentIdentityName()
-        {
-            string result = string.Empty;
-
-            try
-            {
-#if NETSTANDARD2_0
-                result = System.Environment.UserName;
-#else
-                using (var identity = System.Security.Principal.WindowsIdentity.GetCurrent())
-                {
-                    result = identity.Name;
-                }
-#endif
-            }
-            catch (SecurityException exp)
-            {
-                CoreEventSource.Log.LogWindowsIdentityAccessSecurityException(exp.Message);
+                    user: PlatformSingleton.Current.GetCurrentIdentityName());
             }
 
             return result;
