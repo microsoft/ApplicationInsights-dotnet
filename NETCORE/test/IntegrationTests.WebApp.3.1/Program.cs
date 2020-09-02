ff496e99189395963f1920e41263c1c9dcdd39ad
Cijo Thomas <cithomas@microsoft.com>
2020-09-02 13:56:24 -0700

    AspNetCore Integration tests refactor (#2017)
    * AspNetCore Integration tests refactor
    
    * Fix integrationtests.sln
    
    * try both netcore verisons
    
    * tryu order reversa
    
    * typo fix
    
    * remove ssolution build
    
    * use 31 tooling
    
    * sxs dotnet core using rsync
    
    * simplify
    
    * test matrix
    
    * order
    
    * yml
    
    * fix copy paste error
    
    * build and restore before test

--- NETCORE/test/IntegrationTests.WebApp.3.1/Program.cs
+++ NETCORE/test/IntegrationTests.WebApp.3.1/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace IntegrationTests.WebApp
+namespace IntegrationTests.WebApp._3._1
 {
     public class Program
     {
