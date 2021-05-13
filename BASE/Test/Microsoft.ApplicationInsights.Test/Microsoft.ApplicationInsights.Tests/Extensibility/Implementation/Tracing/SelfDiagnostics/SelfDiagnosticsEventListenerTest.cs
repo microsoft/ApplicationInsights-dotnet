a3b2089842bbd9f2b7dd3486778d66e5644ad648
xiang17 <xili9@microsoft.com>
2021-05-13 12:12:44 -0700

    Encode event and write to file in SelfDiagnosticsEventListener (#2265)
    * Encode event and write to file in SelfDiagnosticsEventListener
    
    * Fix compile error

---
+++ BASE/Test/Microsoft.ApplicationInsights.Test/Microsoft.ApplicationInsights.Tests/Extensibility/Implementation/Tracing/SelfDiagnostics/SelfDiagnosticsEventListenerTest.cs
@@ -0,0 +1,89 @@
+﻿namespace Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing.SelfDiagnostics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Tracing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    // SelfDiagnosticsEventListener should be moved from SelfDiagnosticsInternals to SelfDiagnostics.
+    // Pending on https://github.com/microsoft/ApplicationInsights-dotnet/pull/2262
+    // Here still using this old namespace to show less changes in git diff view.
+    using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing.SelfDiagnosticsInternals;
+
+    [TestClass]
+    class SelfDiagnosticsEventListenerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SelfDiagnosticsEventListener_constructor_Invalid_Input()
+        {
+            // no configRefresher object
+            _ = new SelfDiagnosticsEventListener(EventLevel.Error, null);
+        }
+
+        [TestMethod]
+        public void SelfDiagnosticsEventListener_EventSourceSetup_LowerSeverity()
+        {
+            var fileHandlerMock = new Mock<MemoryMappedFileHandler>();
+            var listener = new SelfDiagnosticsEventListener(EventLevel.Error, fileHandlerMock.Object);
+
+            // Emitting a Verbose event. Or any EventSource event with lower severity than Error.
+            CoreEventSource.Log.OperationIsNullWarning();
+            fileHandlerMock.Verify(fileHandler => fileHandler.Write(It.IsAny<byte[]>(), It.IsAny<int>()), Times.Never());
+        }
+
+        [TestMethod]
+        public void SelfDiagnosticsEventListener_EventSourceSetup_HigherSeverity()
+        {
+            var fileHandlerMock = new Mock<MemoryMappedFileHandler>();
+            fileHandlerMock.Setup(fileHandler => fileHandler.Write(It.IsAny<byte[]>(), It.IsAny<int>()));
+            var listener = new SelfDiagnosticsEventListener(EventLevel.Error, fileHandlerMock.Object);
+
+            // Emitting an Error event. Or any EventSource event with higher than or equal to to Error severity.
+            CoreEventSource.Log.InvalidOperationToStopError();
+            fileHandlerMock.Verify(fileHandler => fileHandler.Write(It.IsAny<byte[]>(), It.IsAny<int>()));
+        }
+
+        [TestMethod]
+        public void SelfDiagnosticsEventListener_DateTimeGetBytes()
+        {
+            var fileHandlerMock = new Mock<MemoryMappedFileHandler>();
+            var listener = new SelfDiagnosticsEventListener(EventLevel.Error, fileHandlerMock.Object);
+
+            // Check DateTimeKind of Utc, Local, and Unspecified
+            DateTime[] datetimes = new DateTime[]
+            {
+                DateTime.SpecifyKind(DateTime.Parse("1996-12-01T14:02:31.1234567-08:00"), DateTimeKind.Utc),
+                DateTime.SpecifyKind(DateTime.Parse("1996-12-01T14:02:31.1234567-08:00"), DateTimeKind.Local),
+                DateTime.SpecifyKind(DateTime.Parse("1996-12-01T14:02:31.1234567-08:00"), DateTimeKind.Unspecified),
+                DateTime.UtcNow,
+                DateTime.Now,
+            };
+
+            // Expect to match output string from DateTime.ToString("O")
+            string[] expected = new string[datetimes.Length];
+            for (int i = 0; i < datetimes.Length; i++)
+            {
+                expected[i] = datetimes[i].ToString("O");
+            }
+
+            byte[] buffer = new byte[40 * datetimes.Length];
+            int pos = 0;
+
+            // Get string after DateTimeGetBytes() write into a buffer
+            string[] results = new string[datetimes.Length];
+            for (int i = 0; i < datetimes.Length; i++)
+            {
+                int len = listener.DateTimeGetBytes(datetimes[i], buffer, pos);
+                results[i] = Encoding.Default.GetString(buffer, pos, len);
+                pos += len;
+            }
+
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/SelfDiagnostics/MemoryMappedFileHandler.cs
@@ -0,0 +1,10 @@
+namespace Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing.SelfDiagnostics
+{
+    internal class MemoryMappedFileHandler
+    {
+        public void Write(byte[] buffer, int byteCount)
+        {
+            // TODO. Placeholder for SelfDiagnosticsEventListener
+        }
+    }
+}
