c9735a9766378f3636f7eead2ea93165e904b801
Timothy Mothra Lee <tilee@microsoft.com>
2021-05-20 10:10:12 -0700

    add new project

---
+++ BASE/Test/Microsoft.ApplicationInsights.Test/Microsoft.ApplicationInsights.Authentication.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.ApplicationInsights.Authentication.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Class1
+    {
+        /// <summary>
+        /// The purpose of this test is to verify that the build system discovers and runs this project.
+        /// </summary>
+        [TestMethod]
+        public void HelloWorldTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
