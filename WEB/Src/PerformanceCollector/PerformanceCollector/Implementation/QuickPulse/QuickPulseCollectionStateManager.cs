ae0364dfecec038fbef1b222c3fc4feaea8f3b8a
Timothy Mothra Lee <tilee@microsoft.com>
2021-06-02 15:03:06 -0700

    remove todo

--- WEB/Src/PerformanceCollector/PerformanceCollector/Implementation/QuickPulse/QuickPulseCollectionStateManager.cs
+++ WEB/Src/PerformanceCollector/PerformanceCollector/Implementation/QuickPulse/QuickPulseCollectionStateManager.cs
@@ -110,7 +110,6 @@
             string authToken = null;
             if (this.telemetryConfiguration.CredentialEnvelope != null)
             {
-                // TODO: NEED TO VERIFY THIS IS NOT TRACKED AS A DEPENDENCY
                 authToken = this.telemetryConfiguration.CredentialEnvelope.GetToken();
                 if (authToken == null)
                 {
