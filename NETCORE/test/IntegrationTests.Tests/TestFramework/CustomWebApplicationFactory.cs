12491becd1a725249717a4235fe052d71957712f
TimothyMothra <tilee@microsoft.com>
2021-07-13 15:56:59 -0700

    refactor

--- NETCORE/test/IntegrationTests.Tests/TestFramework/CustomWebApplicationFactory.cs
+++ NETCORE/test/IntegrationTests.Tests/TestFramework/CustomWebApplicationFactory.cs
@@ -12,31 +12,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IntegrationTests.Tests
+namespace IntegrationTests.Tests.TestFramework
 {
-    public class CustomWebApplicationFactory<TStartup>
-    : WebApplicationFactory<TStartup> where TStartup : class
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
         internal ConcurrentBag<ITelemetry> sentItems = new ConcurrentBag<ITelemetry>();
 
-
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-
             builder.ConfigureServices(services =>
             {
-                services.AddLogging(loggingBuilder =>
-                loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft.AspNetCore.DataProtection.KeyManagement.XmlKeyManager", LogLevel.None));
+                services.AddLogging(loggingBuilder => loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft.AspNetCore.DataProtection.KeyManagement.XmlKeyManager", LogLevel.None));
 
                 services.AddSingleton<ITelemetryChannel>(new StubChannel()
                 {
                     OnSend = (item) => this.sentItems.Add(item)
                 });
-                var aiOptions = new ApplicationInsightsServiceOptions();
-                aiOptions.AddAutoCollectedMetricExtractor = false;
-                aiOptions.EnableAdaptiveSampling = false;
-                aiOptions.InstrumentationKey = "ikey";
-                services.AddApplicationInsightsTelemetry(aiOptions);
+
+                services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
+                {
+                    AddAutoCollectedMetricExtractor = false,
+                    EnableAdaptiveSampling = false,
+                    InstrumentationKey = "ikey",
+                });
             });
         }
     }
