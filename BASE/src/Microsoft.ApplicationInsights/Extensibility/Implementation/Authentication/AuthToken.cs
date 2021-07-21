030ebad50145a335c5b3d307409f8c0cc3b872f4
Timothy Mothra <tilee@microsoft.com>
2021-07-20 20:01:45 -0700

    prune public api (#2336)

--- BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Authentication/AuthToken.cs
+++ BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Authentication/AuthToken.cs
@@ -6,7 +6,7 @@
     /// This represents the Azure.Core.AccessToken returned by Azure.Core.TokenCredential.
     /// (https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/core/Azure.Core/src/AccessToken.cs).
     /// </summary>
-    public struct AuthToken
+    internal struct AuthToken : IEquatable<AuthToken>
     {
         /// <summary>
         /// Initializes a new instance of <see cref="AuthToken"/>.
@@ -46,17 +46,12 @@
         public static bool operator !=(AuthToken left, AuthToken right) => !left.Equals(right);
 
         /// <inheritdoc />
-        public override bool Equals(object obj)
-        {
-            if (obj is AuthToken authToken)
-            {
-                return authToken.ExpiresOn == this.ExpiresOn && authToken.Token == this.Token;
-            }
-
-            return false;
-        }
+        public override bool Equals(object obj) => (obj is AuthToken authToken) && this.Equals(authToken);
 
         /// <inheritdoc />
         public override int GetHashCode() => this.Token.GetHashCode() ^ this.ExpiresOn.GetHashCode();
+
+        /// <inheritdoc />
+        public bool Equals(AuthToken other) => other.ExpiresOn == this.ExpiresOn && other.Token == this.Token;
     }
 }
