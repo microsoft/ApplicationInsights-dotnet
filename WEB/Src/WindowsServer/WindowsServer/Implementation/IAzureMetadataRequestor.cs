f51ec520d781b842e6225ce8b6fd0811a49b7d23
TimothyMothra <tilee@microsoft.com>
2021-08-10 17:22:45 -0700

    cleanup

--- WEB/Src/WindowsServer/WindowsServer/Implementation/IAzureMetadataRequestor.cs
+++ WEB/Src/WindowsServer/WindowsServer/Implementation/IAzureMetadataRequestor.cs
@@ -1,6 +1,5 @@
 ﻿namespace Microsoft.ApplicationInsights.WindowsServer.Implementation
 {
-    using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.ApplicationInsights.WindowsServer.Implementation.DataContracts;
