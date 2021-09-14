ae3b3c5e3052f845014edc5b7dcbcad2dfa1bb5a
TimothyMothra <tilee@microsoft.com>
2021-09-13 17:19:54 -0700

    undo

--- NETCORE/test/IntegrationTests.WebApp/Program.cs
+++ NETCORE/test/IntegrationTests.WebApp/Program.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace IntegrationTests.WebApp
 {
