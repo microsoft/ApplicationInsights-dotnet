f34368e576ffebffbb90d20f142cb4883025fe3e
TimothyMothra <tilee@microsoft.com>
2021-11-05 15:48:56 -0700

    cleanup

--- .pipelines/build.cmd
+++ .pipelines/build.cmd
@@ -1,3 +1,3 @@
 cd /D "%~dp0..\"
 
-dotnet build .\ProjectsForSigning.sln -p:PublicRelease=True -p:StableRelease=True -p:BuildServer=True --configuration Release --no-restore || exit /b 1
+dotnet build .\ProjectsForSigning.sln -p:PublicRelease=True -p:StableRelease=True -p:OfficialRelease=True --configuration Release --no-restore || exit /b 1
--- .pipelines/pack.cmd
+++ .pipelines/pack.cmd
@@ -1,3 +1,3 @@
 cd /D "%~dp0..\"
 
-dotnet pack .\ProjectsForSigning.sln -p:PublicRelease=True -p:StableRelease=True -p:BuildServer=True --configuration Release --no-restore --no-build || exit /b 1
+dotnet pack .\ProjectsForSigning.sln -p:PublicRelease=True -p:StableRelease=True -p:OfficialRelease=True --configuration Release --no-restore --no-build || exit /b 1
