7217d6c5803c004589e2b0a02ebfac9e7c410cd0
TimothyMothra <tilee@microsoft.com>
2021-11-17 17:33:58 -0800

    testing consolidating cmd files

--- .pipelines/02_build_buddy.cmd
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0..\"
-
-dotnet build .\ProjectsForSigning.sln -p:OfficialRelease=True --configuration Release --no-restore || exit /b 1
--- .pipelines/03_pack_buddy.cmd
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0..\"
-
-dotnet pack .\ProjectsForSigning.sln -p:OfficialRelease=True --configuration Release --no-restore --no-build || exit /b 1
