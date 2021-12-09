69c07389becd981a8f820165cf72708295ef0fce
TimothyMothra <tilee@microsoft.com>
2021-12-09 13:51:58 -0800

    update for every package name

--- .scripts/tool_UnlistOldPrereleaseVersions.ps1
+++ .scripts/tool_UnlistOldPrereleaseVersions.ps1
@@ -1,11 +1,37 @@
+Param(
+    [Parameter(Mandatory=$true,HelpMessage="Api Key")]
+    [string]
+    $nugetApiKey
+) 
+
+# SUMMARY
+# This script will lookup the version of the latest stable release of a specified package.
+# This script will then attempt to unlist (nuget cli delete) any prerelease versions older than the latest stable.
+
 # https://docs.microsoft.com/en-us/powershell/module/packagemanagement/find-package?view=powershell-7.2
 # https://docs.microsoft.com/en-us/nuget/reference/cli-reference/cli-ref-delete
 
 $nugetSourceUrl = 'https://api.nuget.org/v3/index.json'
-$name = 'Microsoft.ApplicationInsights'
-$nugetApiKey = 'abc'
 
-Clear-Host
+$packageNames = @("Microsoft.ApplicationInsights",
+    "Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel",
+    "Microsoft.AspNet.ApplicationInsights.HostingStartup",
+    "Microsoft.ApplicationInsights.DependencyCollector",
+    "Microsoft.ApplicationInsights.EventCounterCollector",
+    "Microsoft.ApplicationInsights.PerfCounterCollector",
+    "Microsoft.ApplicationInsights.WindowsServer",
+    "Microsoft.ApplicationInsights.Web",
+    "Microsoft.ApplicationInsights.AspNetCore",
+    "Microsoft.ApplicationInsights.WorkerService",
+    "Microsoft.Extensions.Logging.ApplicationInsights",
+    "Microsoft.ApplicationInsights.NLogTarget",
+    "Microsoft.ApplicationInsights.Log4NetAppender",
+    "Microsoft.ApplicationInsights.TraceListener",
+    "Microsoft.ApplicationInsights.DiagnosticSourceListener",
+    "Microsoft.ApplicationInsights.EtwCollector",
+    "Microsoft.ApplicationInsights.EventSourceListener"
+)
+
 
 # Get the latest Nuget.exe from here:
 $nugetPath = ([System.IO.Path]::Combine($PSScriptRoot, '.\NuGet.exe'));
@@ -25,36 +51,37 @@ else {
 }
 
 
+foreach ($name in $packageNames)
+{
+    # Find the latest release
+    Write-Host
+    Write-Host "Querying for latest version..."
+    $latest = Find-Package -Name $name -Source $nugetSourceUrl
+    Write-Host $latest.Name $latest.Version
 
-# Find the latest release
-Write-Host
-Write-Host "Querying for latest version..."
-$latest = Find-Package -Name $name -Source $nugetSourceUrl
-Write-Host $latest.Name $latest.Version
+    # Find all versions older than latest. This filters out any newer prerelease versions.
+    Write-Host
+    Write-Host "Querying for all versions..."
+    $list = Find-Package -Name $name -Source $nugetSourceUrl -AllVersions -MaximumVersion $latest.Version -AllowPrereleaseVersions
 
-# Find all versions older than latest. This filters out any newer prerelease versions.
-Write-Host
-Write-Host "Querying for all versions..."
-$list = Find-Package -Name $name -Source $nugetSourceUrl -AllVersions -MaximumVersion $latest.Version -AllowPrereleaseVersions
+    $testCounter = 0; # For testing, i'm going to delete only one package at a time so i can monitor the logs. 
 
-$testCounter = 0; # For testing, i'm going to delete only one package at a time so i can monitor the logs. 
+    $list | ForEach-Object {
+        # Prerelease versions will contain a hyphen ('-').
+        if ($_.Version.IndexOf('-') -gt -1)
+        {
+            Write-Host $_.Version " PreRelease"
 
-$list | ForEach-Object {
-    # Prerelease versions will contain a hyphen ('-').
-    if ($_.Version.IndexOf('-') -gt -1)
-    {
-        Write-Host $_.Version " PreRelease"
-
-        Write-Host "Unlist: $($_.Name) $($_.Version)"
-        
-        if ($testCounter -eq 0){
-            $testCounter++;
-            & $nugetPath delete $_.Name $_.Version -ApiKey $nugetApiKey -Source $nugetSourceUrl -NonInteractive
+            Write-Host "Unlist: $($_.Name) $($_.Version)"
+            
+            if ($testCounter -eq 0){
+                $testCounter++;
+                & $nugetPath delete $_.Name $_.Version -ApiKey $nugetApiKey -Source $nugetSourceUrl -NonInteractive
+            }
         }
-
-    }
-    else {
-        Write-Host $_.Version;
+        else {
+            Write-Host $_.Version;
+        }
     }
 }
 
