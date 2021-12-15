063af8da5361bce2c602eafadd4fa5ca07275b75
TimothyMothra <tilee@microsoft.com>
2021-12-14 17:07:00 -0800

    testing powershell

--- .pipelines/03_pack.cmd
+++ .pipelines/03_pack.cmd
@@ -1,5 +1,9 @@
 cd /D "%~dp0..\"
 
-dotnet pack .\ProjectsForSigning.sln %* --configuration Release --no-restore --no-build
+dotnet pack .\ProjectsForSigning.sln %* --configuration Release --no-restore --no-build || exit /b 1
 
-.\.scripts\release_GenerateReleaseMetadata.ps1 -artifactsPath .\bin\Release\NuGet -sourcePath .\ -outPath .\bin\Release\NuGet
+
+setlocal enabledelayedexpansion
+powershell.exe -ExecutionPolicy Unrestricted -NoProfile -WindowStyle Hidden -File ".\.scripts\release_GenerateReleaseMetadata.ps1 -artifactsPath .\bin\Release\NuGet -sourcePath .\ -outPath .\bin\Release\NuGet"
+endlocal
+exit /B %ERRORLEVEL%
