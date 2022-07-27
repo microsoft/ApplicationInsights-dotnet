657f54b1afb3868e9da69f4bd71883035b46b0ec
TimothyMothra <tilee@microsoft.com>
2022-07-27 10:18:28 -0700

    remove Net5.0

--- BASE/Test/Microsoft.ApplicationInsights.Test/Microsoft.ApplicationInsights.Tests/Channel/TransmissionTest.cs
+++ BASE/Test/Microsoft.ApplicationInsights.Test/Microsoft.ApplicationInsights.Tests/Channel/TransmissionTest.cs
@@ -222,7 +222,7 @@
                     Assert.AreEqual(206, result.StatusCode);
                     Assert.AreEqual("5", result.RetryAfterHeader);
 
-#if NET5_0_OR_GREATER
+#if NET6_0_OR_GREATER
                     Assert.IsTrue(result.Content == string.Empty);
 #else
                     Assert.IsNull(result.Content);
