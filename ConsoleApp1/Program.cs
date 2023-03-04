a557bbbd1e668057856fec04169e314293dae0cc
Yun-Ting <yunl@microsoft.com>
2023-03-03 17:37:06 -0800

    webappTesting

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Environment.SetEnvironmentVariable("Test1", "Value1");
+            //Environment.SetEnvironmentVariable("fileSize", "2048");
+
+            //var value = Environment.GetEnvironmentVariable("Test1");
+            //var fileSize = String.IsNullOrEmpty(Environment.GetEnvironmentVariable("fileSize")) ? 1024 : Int32.Parse(Environment.GetEnvironmentVariable("fileSize"));
+            //var logLevel = String.IsNullOrEmpty(Environment.GetEnvironmentVariable("logLevel")) ? "Error" : Environment.GetEnvironmentVariable("logLevel");
+
+            //Console.WriteLine(fileSize);
+
+            string input = "StudentId= , FirstName=Jack, LastName=Welch";
+
+            input = Regex.Replace(input, @"\s+", "");
+
+            Dictionary<string, string> keyValuePairs = input.Split(',')
+              .Select(value => value.Split('='))
+              .ToDictionary(pair => pair[0], pair => pair[1]);
+
+            string studentId = keyValuePairs["StudentId"];
+            Console.WriteLine(studentId);
+
+            string firstName = keyValuePairs["FirstName"];
+            Console.WriteLine(firstName);
+            
+            string lastName = keyValuePairs["LastName"];
+            Console.WriteLine(lastName);
+
+            //C:\LogFile\Location
+            //Jack
+            //Welch
+        }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var key = "APPLICATIONINSIGHTS_LOG_DIAGNOSTICS";
+var value = "LogDirectory=C:\\home\\LogFiles\\SelfDiagnostics, FileSize=2048, LogLevel=Verbose";
+Environment.SetEnvironmentVariable(key, value);
+
+builder.Services.AddApplicationInsightsTelemetry();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
