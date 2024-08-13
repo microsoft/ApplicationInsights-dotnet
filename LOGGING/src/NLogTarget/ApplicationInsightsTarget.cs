ea79e58102af5a3355a4f92e228d6e9877aadd8c
Rolf Kristensen <sweaty1@hotmail.com>
2024-08-13 21:46:49 +0200

    NLogTarget - Use FlushAsync instead of synchronous

--- LOGGING/src/NLogTarget/ApplicationInsightsTarget.cs
+++ LOGGING/src/NLogTarget/ApplicationInsightsTarget.cs
@@ -187,18 +187,7 @@ namespace Microsoft.ApplicationInsights.NLogTarget
 
             try
             {
-                this.TelemetryClient.Flush();
-                if (DateTime.UtcNow.AddSeconds(-30) > this.lastLogEventTime)
-                {
-                    // Nothing has been written, so nothing to wait for
-                    asyncContinuation(null);
-                }
-                else
-                {
-                    // Documentation says it is important to wait after flush, else nothing will happen
-                    // https://docs.microsoft.com/azure/application-insights/app-insights-api-custom-events-metrics#flushing-data
-                    System.Threading.Tasks.Task.Delay(TimeSpan.FromMilliseconds(500)).ContinueWith((task) => asyncContinuation(null));
-                }
+                this.TelemetryClient.FlushAsync(System.Threading.CancellationToken.None).ContinueWith(t => asyncContinuation(t.Exception));
             }
             catch (Exception ex)
             {
