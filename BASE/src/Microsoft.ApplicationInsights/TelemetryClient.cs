52c24b429a5af6a1620ce9c314a62b8a7a6856a8
Yorrick Bakker <yorrick.bakker@infosupport.com>
2024-10-19 10:18:25 +0200

    Add GetMetric overloads with dimension params

--- BASE/src/Microsoft.ApplicationInsights/TelemetryClient.cs
+++ BASE/src/Microsoft.ApplicationInsights/TelemetryClient.cs
@@ -1289,6 +1289,67 @@
                         metricConfiguration);
         }
 
+        /// <summary>
+        /// Gets or creates a metric container that you can use to track, aggregate and send metric values.<br />
+        /// Optionally specify a metric configuration to control how the tracked values are aggregated.
+        /// This overload can contain up to 10 custom dimensions.
+        /// </summary>
+        /// <param name="metricId">The ID (name) of the metric.
+        ///   (The namespace specified in <see cref="MetricIdentifier.DefaultMetricNamespace"/> will be used.
+        ///   To specify another namespace, use an overload that takes a <c>MetricIdentifier</c> parameter instead.)</param>
+        /// <param name="metricConfiguration">Determines how tracked values will be aggregated. <br />
+        /// Use presets in <see cref="MetricConfigurations.Common"/> or specify your own settings. </param>
+        /// <returns>A <c>Metric</c> with the specified ID and dimensions. If you call this method several times
+        /// with the same metric ID and dimensions for a given aggregation scope, you will receive the same
+        /// instance of <c>Metric</c>.</returns>
+        /// <exception cref="ArgumentException">If you previously created a metric with the same namespace, ID, dimensions
+        /// and aggregation scope, but with a different configuration. When calling this method to get a previously
+        /// created metric, you can simply avoid specifying any configuration (or specify null) to imply the
+        /// configuration used earlier.</exception>
+        /// <param name="dimensions">Up to 10 custom dimensions for this metric.</param>
+        public Metric GetMetric(
+                            string metricId,
+                            MetricConfiguration metricConfiguration,
+                            params string[] dimensions)
+        {
+            return this.GetOrCreateMetric(
+                        MetricAggregationScope.TelemetryConfiguration,
+                        new MetricIdentifier(MetricIdentifier.DefaultMetricNamespace, metricId, dimensions),
+                        metricConfiguration);
+        }
+        
+        /// <summary>
+        /// Gets or creates a metric container that you can use to track, aggregate and send metric values.<br />
+        /// Optionally specify a metric configuration to control how the tracked values are aggregated.
+        /// This overload can contain up to 10 custom dimensions.
+        /// </summary>
+        /// <param name="metricId">The ID (name) of the metric.
+        ///   (The namespace specified in <see cref="MetricIdentifier.DefaultMetricNamespace"/> will be used.
+        ///   To specify another namespace, use an overload that takes a <c>MetricIdentifier</c> parameter instead.)</param>
+        /// <param name="metricConfiguration">Determines how tracked values will be aggregated. <br />
+        /// Use presets in <see cref="MetricConfigurations.Common"/> or specify your own settings. </param>
+        /// <returns>A <c>Metric</c> with the specified ID and dimensions. If you call this method several times
+        /// with the same metric ID and dimensions for a given aggregation scope, you will receive the same
+        /// instance of <c>Metric</c>.</returns>
+        /// <exception cref="ArgumentException">If you previously created a metric with the same namespace, ID, dimensions
+        /// and aggregation scope, but with a different configuration. When calling this method to get a previously
+        /// created metric, you can simply avoid specifying any configuration (or specify null) to imply the
+        /// configuration used earlier.</exception>
+        /// <param name="aggregationScope">The scope across which the values for the metric are to be aggregated in memory.
+        /// See <see cref="MetricAggregationScope" /> for more info.</param>
+        /// <param name="dimensions">Up to 10 custom dimensions for this metric.</param>
+        public Metric GetMetric(
+            string metricId,
+            MetricConfiguration metricConfiguration,
+            MetricAggregationScope aggregationScope,
+            params string[] dimensions)
+        {
+            return this.GetOrCreateMetric(
+                aggregationScope,
+                new MetricIdentifier(MetricIdentifier.DefaultMetricNamespace, metricId, dimensions),
+                metricConfiguration);
+        }
+
         private Metric GetOrCreateMetric(
                                     MetricAggregationScope aggregationScope,
                                     MetricIdentifier metricIdentifier,
