1bcfa1b0eaf7f7d6b95fcfd9af5f1adcaeec39f9
Simon Cropp <simon.cropp@gmail.com>
2025-01-14 14:52:01 +1100

    add TelemetryProcessorChainTests

---
+++ BASE/Test/Microsoft.ApplicationInsights.Test/Microsoft.ApplicationInsights.Tests/Extensibility/Implementation/TelemetryProcessorChainTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ApplicationInsights.Channel;
+
+
+[TestClass]
+public class TelemetryProcessorChainTests
+{
+    [TestMethod]
+    public void ProcessorsAreDisposed()
+    {
+        var chain = new TelemetryProcessorChain();
+        var processor = new DisposableProcessor();
+        chain.TelemetryProcessors.Add(processor);
+        chain.Dispose();
+        Assert.IsTrue(processor.IsDisposed);
+    }
+
+    class DisposableProcessor : ITelemetryProcessor, IDisposable
+    {
+        public void Process(ITelemetry item)
+        {
+        }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+
+        public bool IsDisposed { get; private set; }
+    }
+}
