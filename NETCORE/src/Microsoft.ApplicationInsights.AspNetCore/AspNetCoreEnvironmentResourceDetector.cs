4ea530420dd75312ac1d0eee1ed143009df35e53
Rajkumar Rangaraj <rajrang@microsoft.com>
2025-11-26 13:49:47 -0800

    Add SDK version labels (#3033)

---
+++ NETCORE/src/Microsoft.ApplicationInsights.AspNetCore/AspNetCoreEnvironmentResourceDetector.cs
@@ -0,0 +1,33 @@
+﻿namespace Microsoft.ApplicationInsights.AspNetCore
+{
+    using System.Collections.Generic;
+    using Microsoft.Extensions.Configuration;
+    using OpenTelemetry.Resources;
+
+    internal class AspNetCoreEnvironmentResourceDetector : IResourceDetector
+    {
+        private const string AspNetCoreEnvironmentPropertyName = "AspNetCoreEnvironment";
+        private readonly IConfiguration configuration;
+
+        public AspNetCoreEnvironmentResourceDetector(IConfiguration configuration)
+        { 
+            this.configuration = configuration;
+        }
+
+        public Resource Detect()
+        {
+            try
+            {
+                var aspNetCoreEnvironment = this.configuration.GetValue<string>("ASPNETCORE_ENVIRONMENT");
+
+                return string.IsNullOrEmpty(aspNetCoreEnvironment)
+                    ? Resource.Empty
+                    : new Resource([new KeyValuePair<string, object>(AspNetCoreEnvironmentPropertyName, aspNetCoreEnvironment)]);
+            }
+            catch
+            {
+                return Resource.Empty;
+            }
+        }
+    }
+}
