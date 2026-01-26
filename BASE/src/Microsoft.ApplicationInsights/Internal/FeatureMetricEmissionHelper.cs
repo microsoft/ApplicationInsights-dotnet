ae9d749932d5436592e740010e95baf6460fec9a
Matthew Sainsbury <msainsbury@microsoft.com>
2026-01-26 09:42:05 -0800

    lint

--- BASE/src/Microsoft.ApplicationInsights/Internal/FeatureMetricEmissionHelper.cs
+++ BASE/src/Microsoft.ApplicationInsights/Internal/FeatureMetricEmissionHelper.cs
@@ -27,7 +27,7 @@ namespace Microsoft.ApplicationInsights.Internal
         private string os;
 
         private StatsbeatFeatures observedFeatures = StatsbeatFeatures.None;
-        private object observedFeaturesLock = new object {};
+        private object observedFeaturesLock = new object { };
 
         private FeatureMetricEmissionHelper(string ciKey, string version)
         {
