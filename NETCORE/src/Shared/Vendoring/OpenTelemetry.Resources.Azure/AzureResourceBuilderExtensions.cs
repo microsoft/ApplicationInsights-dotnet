980944c03855e8ce69bc4ec488868b77a3f42845
Rajkumar Rangaraj <rajrang@microsoft.com>
2026-01-28 13:03:34 -0800

    Vendor OpenTelemetry.Resources.Azure 1.15.0-beta.1 resource detectors

---
+++ NETCORE/src/Shared/Vendoring/OpenTelemetry.Resources.Azure/AzureResourceBuilderExtensions.cs
@@ -0,0 +1,47 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+namespace Microsoft.ApplicationInsights.Shared.Vendoring.OpenTelemetry.Resources
+{
+    using global::OpenTelemetry.Resources;
+    using Microsoft.ApplicationInsights.Shared.Vendoring.OpenTelemetry.Resources.Azure;
+
+    /// <summary>
+    /// Extension methods to simplify registering of Azure resource detectors.
+    /// </summary>
+    internal static class AzureResourceBuilderExtensions
+    {
+        /// <summary>
+        /// Enables Azure App Service resource detector.
+        /// </summary>
+        /// <param name="builder">The <see cref="ResourceBuilder"/> being configured.</param>
+        /// <returns>The instance of <see cref="ResourceBuilder"/> being configured.</returns>
+        public static ResourceBuilder AddAzureAppServiceDetector(this ResourceBuilder builder)
+        {
+            Guard.ThrowIfNull(builder);
+            return builder.AddDetector(new AppServiceResourceDetector());
+        }
+
+        /// <summary>
+        /// Enables Azure VM resource detector.
+        /// </summary>
+        /// <param name="builder">The <see cref="ResourceBuilder"/> being configured.</param>
+        /// <returns>The instance of <see cref="ResourceBuilder"/> being configured.</returns>
+        public static ResourceBuilder AddAzureVMDetector(this ResourceBuilder builder)
+        {
+            Guard.ThrowIfNull(builder);
+            return builder.AddDetector(new AzureVMResourceDetector());
+        }
+
+        /// <summary>
+        /// Enables Azure Container Apps resource detector.
+        /// </summary>
+        /// <param name="builder">The <see cref="ResourceBuilder"/> being configured.</param>
+        /// <returns>The instance of <see cref="ResourceBuilder"/> being configured.</returns>
+        public static ResourceBuilder AddAzureContainerAppsDetector(this ResourceBuilder builder)
+        {
+            Guard.ThrowIfNull(builder);
+            return builder.AddDetector(new AzureContainerAppsResourceDetector());
+        }
+    }
+}
