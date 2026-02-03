7200030451d94a746e91b0eb27c2417cc6804a36
Rajkumar Rangaraj <rajrang@microsoft.com>
2026-02-02 16:56:01 -0800

    Cleanup AssemblyInfo for all 3x projects (#3101)

--- NETCORE/src/Microsoft.ApplicationInsights.AspNetCore/Properties/AssemblyInfo.cs
+++ NETCORE/src/Microsoft.ApplicationInsights.AspNetCore/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.AspNetCore.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("IntegrationTests.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
 [assembly: ComVisible(false)]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "SDK intentionally catch ALL exceptions and never re-throws.")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Unnecessary.")]
--- NETCORE/src/Microsoft.ApplicationInsights.WorkerService/Properties/AssemblyInfo.cs
+++ NETCORE/src/Microsoft.ApplicationInsights.WorkerService/Properties/AssemblyInfo.cs
@@ -2,7 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("Microsoft.ApplicationInsights.WorkerService.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("WorkerIntegrationTests.Tests, PublicKey=" + AssemblyInfo.PublicKey)]
 [assembly: ComVisible(false)]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Unnecessary.")]
