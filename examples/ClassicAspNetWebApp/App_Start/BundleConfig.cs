ce418a2ef2632aa367d842bf45da9370ac2e33d9
Sean Li <xili9@microsoft.com>
2026-06-16 16:55:57 -0700

    Remove use of banned domain

--- examples/ClassicAspNetWebApp/App_Start/BundleConfig.cs
+++ examples/ClassicAspNetWebApp/App_Start/BundleConfig.cs
@@ -44,8 +44,8 @@ namespace ClassicAspNetWebApp
                 {
                     Path = "~/scripts/jquery-3.7.0.min.js",
                     DebugPath = "~/scripts/jquery-3.7.0.js",
-                    CdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.7.0.min.js",
-                    CdnDebugPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.7.0.js"
+                    CdnPath = "https://code.jquery.com/jquery-3.7.1.min.js",
+                    CdnDebugPath = "https://code.jquery.com/jquery-3.7.1.js"
                 });
         }
     }
